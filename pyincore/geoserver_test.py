3ab5c62dcc2642625576ecd6ae3a76526cca6508
Yong Wook Kim <ywkim@illinois.edu>
2017-11-08 14:49:46 -0600

    added test method

--- pyincore/geoserver_test.py
+++ pyincore/geoserver_test.py
@@ -1,8 +1,18 @@
 from geoserver_utils import Geoserver
+from pyincore import DataService
 
 def main():
+    service_url = ''    # dataset service url
+    dataset_id = '' # dataset object id
+    workspace_name = '' # geoserver workspace name
+
     # uploade dataset with id of 9f8d0a2c7d30d278f250a6d to geoserver
-    Geoserver.upload_dataset_geoserver('59f8d0a2c7d30d278f250a6d', 'incore')
+    Geoserver.upload_dataset_geoserver(dataset_id, workspace_name)
+
+    # upload all the dataset in the data repository to geoserver
+    datasets = DataService.get_datasets(service_url)
+    for dataset in datasets:
+        Geoserver.upload_dataset_geoserver(dataset['id'], workspace_name)
 
 if __name__ == "__main__":
     main()
