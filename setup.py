808f6c6ca6c02e3ac733911ff2c7e8c5f6ed77ed
Omar Elabd <oelabd@illinois.edu>
2018-02-09 10:41:58 -0600

    added map_fragilities method, updated setup.py to include dependencies

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from distutils.core import setup
 
-setup(name='pyincore',
-			version='0.1.0',
-			packages=['pyincore'],
-			)
+setup(name = 'pyincore',
+      version = '0.1.0',
+      packages = ['pyincore'], requires = ['fiona', 'rasterio', 'jsonpickle', 'numpy', 'shapely', 'scipy', 'matplotlib']
+      )
