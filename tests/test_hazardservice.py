da1f9e224bab2868986b48ae188ef1da7a53e735
Jong Sung Lee <jonglee1@illinois.edu>
2018-04-17 11:48:05 -0500

    renamed methods in test

--- tests/test_hazardservice.py
+++ tests/test_hazardservice.py
@@ -18,23 +18,23 @@ def hazardsvc():
     return HazardService(client)
 
 
-def test_get_eq_hazard_value(hazardsvc):
+def test_get_earthquake_hazard_value(hazardsvc):
     """
     testing getting hazard value
     https://incore2-services.ncsa.illinois.edu/hazard/api/earthquakes/59f3315ec7d30d4d6741b0bb/value?demandType=0.2+SA&demandUnits=g&siteLat=35.07899&siteLong=-90.0178
     """
     if hazardsvc is None:
         assert False, ".incorepw does not exist!"
-    hval = hazardsvc.get_hazard_value("59f3315ec7d30d4d6741b0bb", "0.2 SA", "g", 35.07899, -90.0178)
+    hval = hazardsvc.get_earthquake_hazard_value("59f3315ec7d30d4d6741b0bb", "0.2 SA", "g", 35.07899, -90.0178)
     assert hval == 0.5322993805448739
 
 
-def test_get_eq_hazard_values(hazardsvc):
+def test_get_earthquake_hazard_values(hazardsvc):
 
     assert False
 
 
-def test_get_eq_hazard_value_set(hazardsvc):
+def test_get_earthquake_hazard_value_set(hazardsvc):
 
     assert False
 
