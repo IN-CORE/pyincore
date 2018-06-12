5702122a87e466c27bc3f513e77dd2622aaf5ccc
vnarah2 <vnarah@illinois.edu>
2018-06-12 16:01:31 -0500

    INCORE-386: WIP Water Facility Damage Analysis. Liquefaction, Uncertainity & Parellelization pending

--- pyincore/analyses/waterfacilitydamage/LifelineUtil.py
+++ pyincore/analyses/waterfacilitydamage/LifelineUtil.py
@@ -11,10 +11,13 @@ class LifelineUtil:
     """
 
     @staticmethod
-    def compute_limit_state_probability(fragility_curves, hazard_val, yvalue, std_dev):
+    def compute_limit_state_probability(fragility_curves, hazard_val, yvalue,
+                                        std_dev):
         ls_probs = collections.OrderedDict()
         for fragility in fragility_curves:
-            ls_probs['ls_'+fragility['description'].lower()] = LifelineUtil.compute_distribution_value(fragility, hazard_val, std_dev)
+            ls_probs['ls_'+fragility['description'].lower()] = \
+                LifelineUtil.compute_distribution_value(fragility, hazard_val,
+                    std_dev)
         return ls_probs
 
     #It's better to move this function to a common util class
@@ -56,52 +59,13 @@ class LifelineUtil:
         #['none', 'slight-mod', 'mod-extens', 'ext-comple', 'complete']
         dmg_intervals['none'] = 1 - ls_probs['ls_slight']
         # what happens when this value is negative ie, moderate > slight
-        dmg_intervals['slight-mod'] = ls_probs['ls_slight'] - ls_probs['ls_moderate']
-        dmg_intervals['mod-extens'] = ls_probs['ls_moderate'] - ls_probs['ls_extensive']
-        dmg_intervals['ext-comple'] = ls_probs['ls_extensive'] - ls_probs['ls_complete']
+        dmg_intervals['slight-mod'] = ls_probs['ls_slight'] - \
+                                      ls_probs['ls_moderate']
+        dmg_intervals['mod-extens'] = ls_probs['ls_moderate'] -\
+                                      ls_probs['ls_extensive']
+        dmg_intervals['ext-comple'] = ls_probs['ls_extensive'] - \
+                                      ls_probs['ls_complete']
         dmg_intervals['complete'] = ls_probs['ls_complete']
 
         return dmg_intervals
 
-
-
-    @staticmethod
-    def get_building_period(num_stories, fragility_set):
-        period = 0.0
-
-        fragility_curve = fragility_set['fragilityCurves'][0]
-        if fragility_curve[
-            'className'] == 'edu.illinois.ncsa.incore.services.fragility.model.PeriodStandardFragilityCurve':
-            period_equation_type = fragility_curve['periodEqnType']
-            if period_equation_type == 1:
-                period = fragility_curve['periodParam0']
-            elif period_equation_type == 3:
-                period = fragility_curve['periodParam0'] * num_stories
-            elif period_equation_type == 3:
-                period = fragility_curve['periodParam1'] * math.pow(fragility_curve['periodParam0'] * num_stories,
-                                                                    fragility_curve['periodParam2'])
-
-        return period
-
-    @staticmethod
-    def get_hazard_demand_type(building, fragility_set, hazard_type):
-        fragility_hazard_type = fragility_set['demandType'].lower()
-        hazard_demand_type = fragility_hazard_type
-
-        if hazard_type.lower() == "earthquake":
-            num_stories = building['properties']['no_stories']
-            #building_period = BuildingUtil.get_building_period(num_stories, fragility_set)
-
-            if fragility_hazard_type.endswith('sa') and fragility_hazard_type != 'sa':
-                # This fixes a bug where demand type is in a format similar to 1.0 Sec Sa
-                if len(fragility_hazard_type.split()) > 2:
-                    building_period = fragility_hazard_type.split()[0]
-                    fragility_hazard_type = "Sa"
-
-            hazard_demand_type = fragility_hazard_type
-
-            # This handles the case where some fragilities only specify Sa, others a specific period of Sa
-            if not hazard_demand_type.endswith('pga'):
-                hazard_demand_type = str(building_period) + " " + fragility_hazard_type
-
-        return hazard_demand_type
--- pyincore/analyses/waterfacilitydamage/waterfacilitydamage.py
+++ pyincore/analyses/waterfacilitydamage/waterfacilitydamage.py
@@ -11,20 +11,25 @@ Options:
     HAZARD      type/id (e.g. earthquake/59f3315ec7d30d4d6741b0bb)
     FRAGILITY   fragility
     MAPPING     fragility mapping
+    UNCERTAINITY Include hazard uncertainity
+    LIQUEFACTION Include Liquefaction in damage calculations
 """
 
 import collections
-from pyincore import InsecureIncoreClient, InventoryDataset, HazardService, FragilityService
+from pyincore import InsecureIncoreClient, InventoryDataset, HazardService,\
+    FragilityService
 from pyincore import GeoUtil, AnalysisUtil
+from pyincore.analyses.waterfacilitydamage.LifelineUtil import LifelineUtil
 from docopt import docopt
 import os
 import csv
 import concurrent.futures
 import multiprocessing
 import traceback
+import random
 
 class WaterFacilityDamage:
-    def __init__(self, client, hazard_service: str ):
+    def __init__(self, client, hazard_service: str):
         # Find hazard type and id
         hazard_service_split = hazard_service.split("/")
         self.hazard_type = hazard_service_split[0]
@@ -34,27 +39,38 @@ class WaterFacilityDamage:
         self.hazardsvc = HazardService(client)
         self.fragilitysvc = FragilityService(client)
 
-    def get_damage(self, inventory_set: dict, fragility: str, fragility_mapping_id: str):
+    def get_damage(self, inventory_set: dict, fragility: str,
+                   fragility_mapping_id: str, uncertainity:bool=False,
+                   liquefaction:bool=False):
         # Get the fragility sets
-        fragility_sets = self.fragilitysvc.map_fragilities(fragility_mapping_id, inventory_set,
-                                                           "pga")
+        if liquefaction:
+            # None available in database now for W-Facility PGD
+            fragility_sets = self.fragilitysvc.map_fragilities(
+                fragility_mapping_id, inventory_set, "pgd")
+        else:
+            fragility_sets = self.fragilitysvc.map_fragilities(
+            fragility_mapping_id, inventory_set, "pga")
+
         output = []
         for facility in inventory_set:
-            facility_output = self.get_water_facility_damage(inventory_set, self.hazard_type,
-                                                self.hazard_dataset_id, self.hazardsvc, fragility_sets)
+            facility_output = self.get_water_facility_damage(facility,
+                self.hazard_type, self.hazard_dataset_id, self.hazardsvc,
+                fragility_sets[facility['id']], uncertainity, liquefaction )
             output.append(facility_output)
 
         return output
 
-    def get_water_facility_damage(self, facility, hazard_type, hazard_dataset_id, hazardsvc, fragility_sets):
+    def get_water_facility_damage(self, facility, hazard_type,
+                                  hazard_dataset_id, hazardsvc, fragility_set,
+                                  uncertainity, liquefaction):
         try:
             fclty_results = collections.OrderedDict()
-            hazardVal = 0.0
-            demand_type = "Unknown"
+            pdg = 0
+            std_dev = 0
 
             dmg_probability = collections.OrderedDict()
 
-            hazard_demand_type = "pga"  # get by hazard type and from fragility set
+            hazard_demand_type = "pga"  # get by hazard type and 4 fragility set
             demand_units = "g"  # Same as above
 
             location = GeoUtil.get_location(facility)
@@ -64,11 +80,59 @@ class WaterFacilityDamage:
                 hazard_val = hazardsvc.get_earthquake_hazard_value(hazard_dataset_id, hazard_demand_type, demand_units,
                                                                    location.y, location.x)
 
+            #limit_states = ['ls-slight', 'ls-moderat', 'ls-extensi', 'ls-complet']
+            #dmg_intervals = ['none', 'slight-mod', 'mod-extens', 'ext-comple', 'complete']
+            if liquefaction:
+                pdg = random.randint(120, 140) #implement API to get pgd
+
+            if uncertainity:
+                std_dev = random.random()
+
+            fragility_yvalue = 1.0
+
+            limit_states = LifelineUtil.compute_limit_state_probability(
+                fragility_set['fragilityCurves'], hazard_val, fragility_yvalue,
+                std_dev)
+            dmg_intervals = LifelineUtil.compute_damage_intervals(limit_states)
+
+            result = collections.OrderedDict()
+            result = {**limit_states, **dmg_intervals} # py 3.5+
+
+
+            for k,v in result.items():
+                result[k] = round(v,2)
+
+            #result = limit_states.copy()
+            #result.update(dmg_intervals)
+
+            metadata = collections.OrderedDict()
+            metadata['guid'] = facility['properties']['guid']
+            metadata['hazardtype'] = hazard_type
+            metadata['hazardval'] = round(hazard_val, 4)
+
+            result = {**metadata, **result}
+            return result
+
         except Exception as e:
             # This prints the type, value and stacktrace of error being handled.
             traceback.print_exc()
             print()
             raise e
+
+    def write_output(self, out_file, output):
+        with open(out_file, 'w') as csv_file:
+            writer = csv.DictWriter(csv_file, dialect="unix",
+                fieldnames=['guid', 'hazardtype','hazardval',
+                            'ls_slight', 'ls_moderate', 'ls_extensive',
+                            'ls_complete',
+                            'none', 'slight-mod', 'mod-extens', 'ext-comple',
+                            'complete'
+                            ])
+
+            writer.writeheader()
+            writer.writerows(output)
+
+        return out_file
 
 if __name__ == '__main__':
         arguments = docopt(__doc__)
@@ -79,7 +143,8 @@ if __name__ == '__main__':
         hazard = arguments['HAZARD']
         fragility = arguments['FRAGILITY']
         mapping_id = arguments['MAPPING']
-        #dmg_ratios = arguments['DMGRATIO']
+        uncertainity = arguments['UNCERTAINITY']
+        liquefaction = arguments['LIQUEFACTION']
 
         # TODO determine the file name
         shp_file = None
@@ -97,8 +162,9 @@ if __name__ == '__main__':
 
             client = InsecureIncoreClient("http://incore2-services.ncsa.illinois.edu:8888", cred[0])
             fcty_dmg = WaterFacilityDamage(client, hazard)
-            fcty_dmg.get_damage(facility_set.inventory_set, fragility, mapping_id )
-
+            dmg_output = fcty_dmg.get_damage(facility_set.inventory_set,
+                fragility, mapping_id, bool(uncertainity), bool(liquefaction))
+            fcty_dmg.write_output('facility-dmg.csv', dmg_output)
 
         except Exception as e:
             print(str(e))
