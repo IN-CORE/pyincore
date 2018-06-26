6cc528d632b481e5af5eb803ee7cf4b1f40987df
candicegjing <candicegjing@gmail.com>
2018-06-26 13:14:14 -0700

    Update hazard service

--- pyincore/analyses/bridgedamage/bridgedamage.py
+++ pyincore/analyses/bridgedamage/bridgedamage.py
@@ -32,16 +32,16 @@ class BridgeDamage:
         self.hazard_type = 'earthquake'
         self.hazard_dataset_id = '5aac069a3342c424fc3fb3e2'
         self.fragilitysvc = FragilityService(self.client)
+        self.mapping_id = "5aa98588949f232724db17fd"
 
     def compute_bridge_damage_analysis(self):
-        mapping_id = "5aa98588949f232724db17fd"
-        fragility_set = self.fragilitysvc.map_fragilities(mapping_id, self.bridges.inventory_set,
+        fragility_set = self.fragilitysvc.map_fragilities(self.mapping_id, self.bridges.inventory_set,
                                                           "Non-Retrofit Fragility ID Code")
         if len(fragility_set) > 0:
             for item in self.bridges.inventory_set:
                 center_point = GeoUtil.get_location(item)
                 id = item['id']
-                hazard_val = self.hazardsvc.get_earthquake_hazard_values(self.hazard_dataset_id,fragility_set[id]['demandType'].lower(), fragility_set[id]['demandUnits'], [center_point])
+                hazard_val = self.hazardsvc.get_earthquake_hazard_value(self.hazard_dataset_id,fragility_set[id]['demandType'].lower(), fragility_set[id]['demandUnits'], center_point.x, center_point.y)
                 print(hazard_val)
 
 
