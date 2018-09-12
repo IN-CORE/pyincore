c1aa0bfb14803f467fd4666a0f63370324f90888
candicegjing <candicegjing@gmail.com>
2018-09-12 11:04:41 -0700

    Resolve merge conflict

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(name = 'pyincore',
       version = '0.2.0',
-      packages = ['pyincore', 'pyincore.analyses', 'pyincore.analyses.buildingdamage',
+      packages = ['pyincore', 'pyincore.analyses', 'pyincore.analyses.buildingdamage', 'pyincore.analyses.waterfacilitydamage',
                   'pyincore.analyses.transportationrecovery', 'pyincore.analyses.pipelinedamage', 'pyincore.analyses.bridgedamage'],
       requires = ['fiona', 'rasterio', 'jsonpickle', 'numpy', 'shapely', 'scipy', 'matplotlib', 'wikidata', 'requests',
                   'networkx', 'pandas', 'folium', 'owslib', 'py_expression_eval'])
