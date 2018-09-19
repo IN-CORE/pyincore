6afdc771bb084ffd814b9fa565e9ce620bb1389a
Yong Wook Kim <ywkim@illinois.edu>
2018-09-19 14:33:45 -0500

    added in setup.py

--- setup.py
+++ setup.py
@@ -3,6 +3,6 @@ from distutils.core import setup
 setup(name = 'pyincore',
       version = '0.2.0',
       packages = ['pyincore', 'pyincore.analyses', 'pyincore.analyses.buildingdamage',
-                  'pyincore.analyses.transportationrecovery', 'pyincore.analyses.pipelinedamage'],
+                  'pyincore.analyses.transportationrecovery', 'pyincore.analyses.pipelinedamage' ,'pyincore.analyses.epnrecoverymodel'],
       requires = ['fiona', 'rasterio', 'jsonpickle', 'numpy', 'shapely', 'scipy', 'matplotlib', 'wikidata', 'requests',
                   'networkx', 'pandas', 'folium', 'owslib', 'py_expression_eval'])
