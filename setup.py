4cc62bef427593575feaf5a96daaff12b3ae1041
Yong Wook Kim <ywkim@illinois.edu>
2018-10-19 14:27:43 -0500

    INCORE-423 "Port tornado epn damage analysis"

--- setup.py
+++ setup.py
@@ -3,9 +3,7 @@ from distutils.core import setup
 setup(name = 'pyincore',
       version = '0.2.0',
       packages = ['pyincore', 'pyincore.analyses', 'pyincore.analyses.buildingdamage',
-                  'pyincore.analyses.transportationrecovery', 'pyincore.analyses.pipelinedamage', 'pyincore.analyses.epnrecoverymodel',
                   'pyincore.analyses.waterfacilitydamage', 'pyincore.analyses.transportationrecovery',
                   'pyincore.analyses.pipelinedamage', 'pyincore.analyses.bridgedamage',
-                  'pyincore.analyses.stochastic_population'],
-      requires = ['fiona', 'rasterio', 'jsonpickle', 'numpy', 'shapely', 'scipy', 'matplotlib', 'wikidata', 'requests',
-                  'networkx', 'pandas', 'folium', 'owslib', 'py_expression_eval', 'rasterstats'])
+                  'pyincore.analyses.stochastic_population', 'pyincore.analyses.tornadoepndamage'],
+      requires = ['fiona', 'rasterio', 'jsonpickle', 'numpy', 'shapely', 'scipy', 'matplotlib', 'wikidata', 'requests', 'networkx', 'pandas', 'pyproj', 'folium', 'owslib', 'py_expression_eval', 'rtree' ])
