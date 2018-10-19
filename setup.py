2cfa9165c5f15937fb46e6ecdcb349277b302161
Indira Gutierrez Polo <indiragp@illinois.edu>
2018-10-19 14:17:48 -0500

    Updating Stochastic Population Inventory

--- setup.py
+++ setup.py
@@ -2,7 +2,9 @@ from distutils.core import setup
 
 setup(name = 'pyincore',
       version = '0.2.0',
-      packages = ['pyincore', 'pyincore.analyses', 'pyincore.analyses.buildingdamage', 'pyincore.analyses.waterfacilitydamage',
-                  'pyincore.analyses.transportationrecovery', 'pyincore.analyses.pipelinedamage', 'pyincore.analyses.bridgedamage'],
+      packages = ['pyincore', 'pyincore.analyses', 'pyincore.analyses.buildingdamage',
+                  'pyincore.analyses.waterfacilitydamage', 'pyincore.analyses.transportationrecovery',
+                  'pyincore.analyses.pipelinedamage', 'pyincore.analyses.bridgedamage',
+                  'pyincore.analyses.stochastic_population'],
       requires = ['fiona', 'rasterio', 'jsonpickle', 'numpy', 'shapely', 'scipy', 'matplotlib', 'wikidata', 'requests',
                   'networkx', 'pandas', 'folium', 'owslib', 'py_expression_eval'])
