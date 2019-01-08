36b9d097e254af6c96a847d8cafcdd1367a8ea04
vnarah2 <vnarah@illinois.edu>
2019-01-08 12:27:12 -0600

    INCORE-612: wip Refactoring done. Need to revisit run method

--- pyincore/analyses/stochastic_population/stochastic_population_allocation.py
+++ pyincore/analyses/stochastic_population/stochastic_population_allocation.py
@@ -2,37 +2,139 @@
 import os
 import pandas as pd
 import numpy as np
+from pyincore import BaseAnalysis
 
 
-class StochasticPopulationAllocation:
-    def __init__(self, address_point_inventory, building_inventory, population_inventory,
-                 output_name, output_directory, seed, iterations, intermediate_files):
-        self.output_name = output_name
-        self.output_directory = output_directory
-        self.seed = seed
-        self.iterations = iterations
-        self.intermediate_files = intermediate_files
-        self.intermediate_files = True #TODO: what is marked as intermediate are the final outputs
-        if os.path.isfile(address_point_inventory):
-            self.address_point_inventory = self.load_csv_file(address_point_inventory)
+class StochasticPopulationAllocation(BaseAnalysis):
+    def __init__(self, incore_client):
+        # self.po
+        # self.output_name = output_name
+        # self.seed = seed
+        # self.iterations = iterations
+        # self.intermediate_files = intermediate_files
+        # self.intermediate_files = True #TODO: what is marked as intermediate are the final outputs
+        # if os.path.isfile(address_point_inventory):
+        #     self.address_point_inventory = self.load_csv_file(address_point_inventory)
+        #
+        # if os.path.isfile(building_inventory):
+        #     self.building_inventory = self.load_csv_file(building_inventory)
+        #
+        # # if os.path.isfile(critical_infrastructure_inventory):
+        # #     self.critical_infrastructure_inventory = self.load_csv_file(critical_infrastructure_inventory)
+        #
+        # if os.path.isfile(population_inventory):
+        #     self.population_inventory = self.load_csv_file(population_inventory)
 
-        if os.path.isfile(building_inventory):
-            self.building_inventory = self.load_csv_file(building_inventory)
+        super(StochasticPopulationAllocation, self).__init__(incore_client)
 
-        # if os.path.isfile(critical_infrastructure_inventory):
-        #     self.critical_infrastructure_inventory = self.load_csv_file(critical_infrastructure_inventory)
+    def get_spec(self):
+        return {
+            'name': 'stochastic-population-allocation',
+            'description': 'Stochastic Population Allocation Analysis',
+            'input_parameters': [
+                {
+                    'id': 'result_name',
+                    'required': False,
+                    'description': 'Result CSV dataset name',
+                    'type': str
+                },
+                {
+                    'id': 'seed',
+                    'required': True,
+                    'description': 'Initial Seed for the Stochastic model',
+                    'type': int
+                },
+                {
+                    'id': 'iterations',
+                    'required': True,
+                    'description': 'No of iterations to perform the stochastic model on',
+                    'type': int
+                }
+            ],
+            'input_datasets': [
+                {
+                    'id': 'population_inventory',
+                    'required': True,
+                    'description': 'Population Inventory CSV data, aka Census Block data. Corresponds to a possible '
+                                   'occupied housing unit, vacant housing unit, or a group quarters',
+                    'type': ['ergo:censusBlockPopulation']
+                },
+                {
+                    'id': 'address_point_inventory',
+                    'required': True,
+                    'description': 'CSV dataset of address locations available in a block. Corresponds to a '
+                                   'specific address where a housing unit or group quarters could be assigned',
+                    'type': ['ergo:addressPoints']
+                },
+                {
+                    'id': 'building_inventory',
+                    'required': True,
+                    'description': 'Building Inventory CSV dataset for each building/structure. A structure '
+                                   'can have multiple addresses',
+                    'type': ['ergo:buildingInventory']
+                }
+            ],
+            'output_datasets': [
+                {
+                    'id': 'result',
+                    'parent_type': 'population_block', #TODO: Is parent_type needed?
+                    'description': 'A csv file with the merged dataset of the inputs, aka Stochastic'
+                                   'Population Allocation',
+                    'type': 'csv'
+                }
+            ]
+        }
 
-        if os.path.isfile(population_inventory):
-            self.population_inventory = self.load_csv_file(population_inventory)
+    def run(self):
+        """Merges Population Inventory, Address Point Inventory and Building Inventory.
+         The results of this analysis are aggregated per structure/building
+
+        Returns:
+               bool: True if successful, False otherwise
+        """
+
+        # Get seed
+        seed = self.get_parameter("seed")
+
+        # Get Iterations
+        iterations = self.get_parameter("iterations")
+
+        # Get desired result name
+        result_name = self.get_parameter("result_name")
+
+        # Population Inventory Dataset
+        pop_inv_file = self.get_input_dataset("population_inventory").get_file_path('csv')
+        pop_inv = self.load_csv_file(pop_inv_file)
+
+        # Address Point Inventory Dataset
+        addr_point_inv_file = self.get_input_dataset("address_point_inventory").get_file_path('csv')
+        addr_point_inv = self.load_csv_file(addr_point_inv_file)
+
+        # Building Inventory dataset
+        bg_inv_file = self.get_input_dataset("building_inventory").get_file_path('csv')
+        bg_inv = self.load_csv_file(bg_inv_file)
+
+        csv_source = "dataframe"
+
+        for i in range(iterations):
+            seed_i = seed + i
+            sorted_population_address_inventory = self.get_iteration_stochastic_allocation(
+                pop_inv, addr_point_inv, bg_inv, seed_i)
+            temp_output_file = result_name + "_" + str(seed_i) + ".csv"
+
+            self.set_result_csv_data("result", sorted_population_address_inventory, temp_output_file, csv_source)
+
+        return True
+
 
     # building setter with updated Pandas DataFrame
     def set_building_inv(self, building_inv: pd.DataFrame):
         self.building_inventory = building_inv
 
-    def prepare_population_inventory(self, seed):
-        size_row, size_col = self.population_inventory.shape
+    def prepare_population_inventory(self,population_inventory, seed):
+        size_row, size_col = population_inventory.shape
         np.random.seed(seed)
-        sorted_population0 = self.population_inventory.sort_values(by=["lvtypuntid"])
+        sorted_population0 = population_inventory.sort_values(by=["lvtypuntid"])
 
         # Create Random merge order for population inventory
         random_order_population = np.random.uniform(0, 1, size_row)
@@ -50,7 +152,7 @@ class StochasticPopulationAllocation:
                                                             ascending=[True, False])
         return sorted_population2
 
-    def merge_infrastructure_inventory(self):
+    def merge_infrastructure_inventory(self, address_point_inventory, building_inventory ):
         """Merge order to Building, Water and Address inventories.
 
             Args:
@@ -59,8 +161,8 @@ class StochasticPopulationAllocation:
             Returns:
                 output (pd.DataFrame)
         """
-        sorted_pnt_0 = self.address_point_inventory.sort_values(by=["strctid"])
-        sorted_bld_0 = self.building_inventory.sort_values(by=["strctid"])
+        sorted_pnt_0 = address_point_inventory.sort_values(by=["strctid"])
+        sorted_bld_0 = building_inventory.sort_values(by=["strctid"])
 
         addresspt_building_inv = pd.merge(sorted_bld_0, sorted_pnt_0,
                                           how='outer', on="strctid",
@@ -145,52 +247,49 @@ class StochasticPopulationAllocation:
         output = sorted_population_address_inventory[sorted_population_address_inventory["exists3"] == 'both']
         return output
 
-    def get_iteration_stochastic_allocation(self, seed):
-        sorted_population = self.prepare_population_inventory(seed)
+    def get_iteration_stochastic_allocation(self, population_inventory, address_point_inventory, building_inventory, seed):
+        sorted_population = self.prepare_population_inventory(population_inventory, seed)
 
         # merge infrastructure-related inventories (building, address point and infrastructure (water)
-        critical_building_inv = self.merge_infrastructure_inventory()
+        critical_building_inv = self.merge_infrastructure_inventory(address_point_inventory, building_inventory)
         sorted_infrastructure = self.prepare_infrastructure_inventory(seed, critical_building_inv)
 
         output = self.merge_inventories(sorted_population, sorted_infrastructure)
         return output
 
-    def get_stochastic_population_allocation(self):
-        output_directory = self.output_directory
-        if not os.path.exists(output_directory):
-            os.makedirs(output_directory)
-
-        output = pd.DataFrame()
-        #unique_skeleton_ids = self.critical_infrastructure_inventory['wtrdnd2'].unique()
-
-        # Avoids error indicating that a DataFrame was updated in place.
-        pd.options.mode.chained_assignment = None
-        for i in range(self.iterations):
-            seed_i = self.seed + i
-
-            sorted_population_address_inventory = self.get_iteration_stochastic_allocation(seed_i)
-            output_item = {"i": i, "seed_i": seed_i, "other": 0}
-
-            # for skeleton_id in unique_skeleton_ids:
-            #     output_item[skeleton_id] = 0
-            #
-            # for idx, item in sorted_population_address_inventory.iterrows():
-            #
-            #     if item['exists3'] == 'both':
-            #         skeleton_id = item['wtrdnd2']
-            #         if np.isnan(skeleton_id):
-            #             skeleton_id = "other"
-            #         output_item[skeleton_id] += item["numprec"]
-            #
-            # output = output.append(pd.Series(output_item, name=str(i)))
-            # if self.intermediate_files:
-            temp_output_file = os.path.join(output_directory, self.output_name + "_" + str(seed_i) + ".csv")
-            sorted_population_address_inventory.to_csv(temp_output_file, mode="w+", index=False)
-
-            # output_file = os.path.join(output_directory, self.output_name + ".csv")
-            # output.to_csv(output_file, mode="w+", index=False)
-        #return output
-        return True
+    # def get_stochastic_population_allocation(self):
+    #
+    #     output = pd.DataFrame()
+    #     #unique_skeleton_ids = self.critical_infrastructure_inventory['wtrdnd2'].unique()
+    #
+    #     # Avoids error indicating that a DataFrame was updated in place.
+    #     pd.options.mode.chained_assignment = None
+    #     for i in range(self.iterations):
+    #         seed_i = self.seed + i
+    #
+    #         sorted_population_address_inventory = self.get_iteration_stochastic_allocation(seed_i)
+    #         output_item = {"i": i, "seed_i": seed_i, "other": 0}
+    #
+    #         # for skeleton_id in unique_skeleton_ids:
+    #         #     output_item[skeleton_id] = 0
+    #         #
+    #         # for idx, item in sorted_population_address_inventory.iterrows():
+    #         #
+    #         #     if item['exists3'] == 'both':
+    #         #         skeleton_id = item['wtrdnd2']
+    #         #         if np.isnan(skeleton_id):
+    #         #             skeleton_id = "other"
+    #         #         output_item[skeleton_id] += item["numprec"]
+    #         #
+    #         # output = output.append(pd.Series(output_item, name=str(i)))
+    #         # if self.intermediate_files:
+    #         temp_output_file = self.output_name + "_" + str(seed_i) + ".csv"
+    #         sorted_population_address_inventory.to_csv(temp_output_file, mode="w+", index=False)
+    #
+    #         # output_file = os.path.join(output_directory, self.output_name + ".csv")
+    #         # output.to_csv(output_file, mode="w+", index=False)
+    #     #return output
+    #     return True
 
     @staticmethod
     def load_csv_file(file_name):
--- pyincore/analyses/stochastic_population/teststochastic.py
+++ pyincore/analyses/stochastic_population/teststochastic.py
@@ -34,7 +34,7 @@ def run_without_base_class():
     spi = StochasticPopulationAllocation(address_point_inventory_file,
         building_inventory_file,
         #critical_infrastructure_inventory_file,
-        population_inventory_file, output_name_file, base_path, initial_seed,
+        population_inventory_file, output_name_file, initial_seed,
         n_iterations,
         generate_temporary_files)
     spi.get_stochastic_population_allocation()
@@ -42,9 +42,32 @@ def run_without_base_class():
 
 
 def run_with_base_class():
+    client = InsecureIncoreClient("http://incore2-services.ncsa.illinois.edu:8888", "incrtest")
+
+    pop_inv = "5c34da82c5c0e40670b77633"
+    addr_point_inv = "5c34db03c5c0e40670b77637"
+    bg_inv = "5c34db0ec5c0e40670b7763b"
+
+    result_name = "stochastic-allocation"
+    seed = 1238
+    iterations = 5
+
+    spa = StochasticPopulationAllocation(client)
+    spa.load_remote_input_dataset("population_inventory", pop_inv)
+    spa.load_remote_input_dataset("address_point_inventory", addr_point_inv)
+    spa.load_remote_input_dataset("building_inventory", bg_inv)
+
+    spa.set_parameter("result_name", result_name)
+    spa.set_parameter("seed", seed)
+    spa.set_parameter("iterations", iterations)
+
+    spa.run_analysis()
+
+
     return
 
 
 
 if __name__ == '__main__':
-    run_without_base_class()
+    #run_without_base_class()
+    run_with_base_class()
