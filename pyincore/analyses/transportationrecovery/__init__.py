d60e5c1e22ea89264eea826a29c027f53172f8d8
diego <diegoac2@illinois.edu>
2019-02-11 22:52:36 -0600

    Added license headers

--- pyincore/analyses/transportationrecovery/__init__.py
+++ pyincore/analyses/transportationrecovery/__init__.py
@@ -1,11 +1,9 @@
-"""
+# Copyright (c) 2019 University of Illinois and others. All rights reserved.
+#
+# This program and the accompanying materials are made available under the
+# terms of the Mozilla Public License v2.0 which accompanies this distribution,
+# and is available at https://www.mozilla.org/en-US/MPL/2.0/
 
-Copyright (c) 2019 University of Illinois and others.  All rights reserved.
-This program and the accompanying materials are made available under the
-terms of the Mozilla Public License v2.0 which accompanies this distribution,
-and is available at https://www.mozilla.org/en-US/MPL/2.0/
-
-"""
 
 from pyincore.analyses.transportationrecovery.freeflow_traveltime import traveltime_freeflow
 from pyincore.analyses.transportationrecovery.network_reconstruction import nw_reconstruct
