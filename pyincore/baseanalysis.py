8fc2eb0d0f147b9461d6282ff54148f0de35e54a
diego <diegoac2@illinois.edu>
2019-02-18 11:21:36 -0600

    fixed a header

--- pyincore/baseanalysis.py
+++ pyincore/baseanalysis.py
@@ -1,11 +1,9 @@
-"""pyincore.baseanalysis
+# Copyright (c) 2019 University of Illinois and others. All rights reserved.
+#
+# This program and the accompanying materials are made available under the
+# terms of the Mozilla Public License v2.0 which accompanies this distribution,
+# and is available at https://www.mozilla.org/en-US/MPL/2.0/
 
-Copyright (c) 2017 University of Illinois and others.  All rights reserved.
-This program and the accompanying materials are made available under the
-terms of the BSD-3-Clause which accompanies this distribution,
-and is available at https://opensource.org/licenses/BSD-3-Clause
-
-"""
 # TODO: exception handling for validation and set methods
 from pyincore import DataService, AnalysisUtil
 from pyincore.dataset import Dataset
