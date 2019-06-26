8c35eb5148b777133320ba27c8491d57378c286d
Michal Ondrejcek <ondrejce@illinois.edu>
2019-06-26 10:29:43 -0500

    INCORE-829-social-science-models-name-changes

--- pyincore/analyses/housingunitallocation/housingunitallocation.py
+++ pyincore/analyses/housingunitallocation/housingunitallocation.py
@@ -2,20 +2,19 @@
 # terms of the Mozilla Public License v2.0 which accompanies this distribution,
 # and is available at https://www.mozilla.org/en-US/MPL/2.0/
 
-import os
-import pandas as pd
 import numpy as np
+import pandas as pd
 from pyincore import BaseAnalysis
 
 
-class StochasticPopulationAllocation(BaseAnalysis):
+class HousingUnitAllocation(BaseAnalysis):
     def __init__(self, incore_client):
-        super(StochasticPopulationAllocation, self).__init__(incore_client)
+        super(HousingUnitAllocation, self).__init__(incore_client)
 
     def get_spec(self):
         return {
-            'name': 'stochastic-population-allocation',
-            'description': 'Stochastic Population Allocation Analysis',
+            'name': 'housing-unit-allocation',
+            'description': 'Probabilistic Housing Unit Allocation Analysis',
             'input_parameters': [
                 {
                     'id': 'result_name',
@@ -26,30 +25,30 @@ class StochasticPopulationAllocation(BaseAnalysis):
                 {
                     'id': 'seed',
                     'required': True,
-                    'description': 'Initial Seed for the Stochastic model',
+                    'description': 'Initial Seed for the Probabilistic model',
                     'type': int
                 },
                 {
                     'id': 'iterations',
                     'required': True,
-                    'description': 'No of iterations to perform the stochastic model on',
+                    'description': 'No of iterations to perform the probabilistic model on',
                     'type': int
                 }
             ],
             'input_datasets': [
                 {
-                    'id': 'population_inventory',
+                    'id': 'housing_unit_inventory',
                     'required': True,
-                    'description': 'Population Inventory CSV data, aka Census Block data. Corresponds to a possible '
+                    'description': 'Housing Unit Inventory CSV data, aka Census Block data. Corresponds to a possible '
                                    'occupied housing unit, vacant housing unit, or a group quarters',
-                    'type': ['ergo:censusBlockPopulation']
+                    'type': ['incore:housingUnitInventory']
                 },
                 {
                     'id': 'address_point_inventory',
                     'required': True,
                     'description': 'CSV dataset of address locations available in a block. Corresponds to a '
                                    'specific address where a housing unit or group quarters could be assigned',
-                    'type': ['ergo:addressPoints']
+                    'type': ['incore:addressPoints']
                 },
                 {
                     'id': 'building_inventory',
@@ -62,20 +61,21 @@ class StochasticPopulationAllocation(BaseAnalysis):
             'output_datasets': [
                 {
                     'id': 'result',
-                    'description': 'A csv file with the merged dataset of the inputs, aka Stochastic'
-                                   'Population Allocation',
-                    'type': 'incore:PopAllocation'
+                    'description': 'A csv file with the merged dataset of the inputs, aka Probabilistic'
+                                   'Housing Unit Allocation',
+                    'type': 'incore:housingUnitAllocation'
                 }
             ]
         }
 
     def run(self):
-        """Merges Population Inventory, Address Point Inventory and Building Inventory.
+        """Merges Housing Unit Inventory, Address Point Inventory and Building Inventory.
          The results of this analysis are aggregated per structure/building. Generates
          one csv result per iteration.
 
         Returns:
                bool: True if successful, False otherwise
+
         """
 
         # Get seed
@@ -87,8 +87,8 @@ class StochasticPopulationAllocation(BaseAnalysis):
         # Get desired result name
         result_name = self.get_parameter("result_name")
 
-        # Population Inventory Dataset
-        pop_inv = self.get_input_dataset("population_inventory").get_dataframe_from_csv()
+        # Housing Unit Inventory Dataset
+        pop_inv = self.get_input_dataset("housing_unit_inventory").get_dataframe_from_csv()
 
         # Address Point Inventory Dataset
         addr_point_inv = self.get_input_dataset("address_point_inventory").get_dataframe_from_csv()
@@ -100,43 +100,56 @@ class StochasticPopulationAllocation(BaseAnalysis):
 
         for i in range(iterations):
             seed_i = seed + i
-            sorted_population_address_inventory = self.get_iteration_stochastic_allocation(
+            sorted_housing_unit_address_inventory = self.get_iteration_probabilistic_allocation(
                 pop_inv, addr_point_inv, bg_inv, seed_i)
             temp_output_file = result_name + "_" + str(seed_i) + ".csv"
 
-            self.set_result_csv_data("result", sorted_population_address_inventory, temp_output_file, csv_source)
-
+            self.set_result_csv_data("result",
+                                     sorted_housing_unit_address_inventory,
+                                     temp_output_file, csv_source)
         return True
 
-    def prepare_population_inventory(self,population_inventory, seed):
-        size_row, size_col = population_inventory.shape
+    def prepare_housing_unit_inventory(self, housing_unit_inventory, seed):
+        """Merge order to Building and Address inventories.
+
+        Args:
+            housing_unit_inventory (pd.DataFrame): Housing unit inventory
+            seed (int): random number generator seed for reproducibility
+
+        Returns:
+            pd.DataFrame: Sorted housing unit inventory
+
+        """
+        size_row, size_col = housing_unit_inventory.shape
         np.random.seed(seed)
-        sorted_population0 = population_inventory.sort_values(by=["lvtypuntid"])
+        sorted_housing_unit0 = housing_unit_inventory.sort_values(by=["lvtypuntid"])
 
-        # Create Random merge order for population inventory
-        random_order_population = np.random.uniform(0, 1, size_row)
+        # Create Random merge order for housing unit inventory
+        random_order_housing_unit = np.random.uniform(0, 1, size_row)
 
-        sorted_population0["randomblock"] = random_order_population
+        sorted_housing_unit0["randomblock"] = random_order_housing_unit
 
         #  gsort BlockID -LiveTypeUnit Tenure randomaorderpop
-        sorted_population1 = sorted_population0.sort_values(by=["blockid", "ownershp", "vacancy", "randomblock"],
-                                                            ascending=[True, True, True, True])
+        sorted_housing_unit1 = sorted_housing_unit0.sort_values(by=["blockid", "ownershp", "vacancy", "randomblock"],
+                                                                ascending=[True, True, True, True])
 
         # by BlockID: gen RandomMergeOrder = _n
-        sorted_population1["mergeorder"] = sorted_population1.groupby(["blockid"]).cumcount()
+        sorted_housing_unit1["mergeorder"] = sorted_housing_unit1.groupby(["blockid"]).cumcount()
 
-        sorted_population2 = sorted_population1.sort_values(by=["blockid", "mergeorder"],
-                                                            ascending=[True, False])
-        return sorted_population2
+        sorted_housing_unit2 = sorted_housing_unit1.sort_values(by=["blockid", "mergeorder"],
+                                                                ascending=[True, False])
+        return sorted_housing_unit2
 
     def merge_infrastructure_inventory(self, address_point_inventory, building_inventory ):
         """Merge order to Building and Address inventories.
 
         Args:
-            self: for chaining
+            address_point_inventory (pd.DataFrame): address point inventory
+            building_inventory (pd.DataFrame): building inventory
 
         Returns:
-            output (pd.DataFrame)
+            pd.DataFrame: merged address and building inventories
+
         """
         sorted_pnt_0 = address_point_inventory.sort_values(by=["strctid"])
         sorted_bld_0 = building_inventory.sort_values(by=["strctid"])
@@ -157,12 +170,12 @@ class StochasticPopulationAllocation(BaseAnalysis):
         seed value.
 
         Args:
-            self: for chaining
-            seed_i (int):              Seed for reproducibility
-            critical_bld_inv (pd.DataFrame): merged inventories
+            seed_i (int): random number generator seed for reproducibility
+            critical_bld_inv (pd.DataFrame): Merged inventories
 
         Returns:
-            output (pd.DataFrame)
+            pd.DataFrame: Sorted merged critical infrastructure
+
         """
         size_row, size_col = critical_bld_inv.shape
 
@@ -182,42 +195,54 @@ class StochasticPopulationAllocation(BaseAnalysis):
                                                               ascending=[True, False])
         return sort_critical_bld_2
 
-    def merge_inventories(self, sorted_population: pd.DataFrame, sorted_infrastructure: pd.DataFrame):
-        """Merge (Sorted) Population Inventory and (Sorted) Infrastrucutre Inventory.
+    def merge_inventories(self, sorted_housing_unit: pd.DataFrame, sorted_infrastructure: pd.DataFrame):
+        """Merge (Sorted) Housing Unit Inventory and (Sorted) Infrastructure Inventory.
 
         Args:
-            self: for chaining
-            sorted_population (pd.DataFrame):  Sorted population inventory
-            sorted_infrastructure (pd.DataFrame):  Sorted infrastrucutre inventory. This includes
-                Building inventory, Address point inventory and Critical (Water) Infrastrucutre
+            sorted_housing_unit (pd.DataFrame):  Sorted Housing Unit Inventory
+            sorted_infrastructure (pd.DataFrame):  Sorted infrastructure inventory. This includes
+                Building inventory and Address point inventory.
 
         Returns:
-            final_merge_inv (pd.DataFrame): final merge of all four inventories
+            pd.DataFrame: Final merge of all four inventories
+
         """
-        population_address_point_inventory = pd.merge(sorted_infrastructure, sorted_population,
-                                                      how='outer', left_on=["blockid", "mergeorder"],
-                                                      right_on=["blockid", "mergeorder"],
-                                                      sort=True, suffixes=("_x", "_y"),
-                                                      copy=True, indicator="exists3", validate="1:1")
+        housing_unit_address_point_inventory = pd.merge(sorted_infrastructure, sorted_housing_unit,
+                                                        how='outer', left_on=["blockid", "mergeorder"],
+                                                        right_on=["blockid", "mergeorder"],
+                                                        sort=True, suffixes=("_x", "_y"),
+                                                        copy=True, indicator="exists3", validate="1:1")
 
         # check for duplicate columns from merge
-        population_address_point_inventory = self.compare_merges(sorted_population.columns,
-                                                                 sorted_infrastructure.columns,
-                                                                 population_address_point_inventory)
+        housing_unit_address_point_inventory = self.compare_merges(sorted_housing_unit.columns,
+                                                                   sorted_infrastructure.columns,
+                                                                   housing_unit_address_point_inventory)
 
-        sorted_population_address_inventory = population_address_point_inventory.sort_values(
+        sorted_housing_unit_address_inventory = housing_unit_address_point_inventory.sort_values(
             by=["lvtypuntid"])
 
-        output = sorted_population_address_inventory[sorted_population_address_inventory["exists3"] == 'both']
+        output = sorted_housing_unit_address_inventory[sorted_housing_unit_address_inventory["exists3"] == 'both']
         return output
 
-    def get_iteration_stochastic_allocation(self, population_inventory, address_point_inventory, building_inventory, seed):
-        sorted_population = self.prepare_population_inventory(population_inventory, seed)
+    def get_iteration_probabilistic_allocation(self, housing_unit_inventory, address_point_inventory, building_inventory, seed):
+        """Merge inventories
+
+        Args:
+            housing_unit_inventory (pd.DataFrame): Housing Unit Inventory
+            address_point_inventory (pd.DataFrame): Address Point inventory
+            building_inventory (pd.DataFrame): Building inventory
+            seed (int): random number generator seed for reproducibility
+
+            Returns:
+                pd.DataFrame: Merged table
+
+        """
+        sorted_housing_unit = self.prepare_housing_unit_inventory(housing_unit_inventory, seed)
 
         critical_building_inv = self.merge_infrastructure_inventory(address_point_inventory, building_inventory)
         sorted_infrastructure = self.prepare_infrastructure_inventory(seed, critical_building_inv)
 
-        output = self.merge_inventories(sorted_population, sorted_infrastructure)
+        output = self.merge_inventories(sorted_housing_unit, sorted_infrastructure)
         return output
 
     # utility functions
@@ -226,13 +251,13 @@ class StochasticPopulationAllocation(BaseAnalysis):
         It assumes that suffixes are _x and _y
 
         Args:
-            self:                       for chaining
             table1_cols (list):         columns in table 1
             table2_cols (list):         columns in table 2
             table_merged (pd.DataFrame):merged table
 
             Returns:
-                table_merged (pd.DataFrame)
+                pd.DataFrame: Merged table
+
         """
         match_column = set(table1_cols).intersection(table2_cols)
         for col in match_column:
@@ -247,14 +272,14 @@ class StochasticPopulationAllocation(BaseAnalysis):
         with the base name and drop the other.
 
         Args:
-            self:                      for chaining.
             table (pd.DataFrame):      Data Frame table
             col1 (pd.Series):          Column 1
             col2 (pd.Series):          Column 2
             drop (bool):               rename and drop column
 
         Returns:
-            output (pd.DataFrame)
+            pd.DataFrame: Table with True/False column
+
         """
         # Values in columns match or not, add True/False column
         table.loc[table[col1] == table[col2], col1+"-"+col2] = True
