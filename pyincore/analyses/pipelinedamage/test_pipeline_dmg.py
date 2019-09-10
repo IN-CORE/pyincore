c91094a8bf6097d38f0cb68e47e2531353b0e7f5
Chen Wang <cwang138@illinois.edu>
2019-09-10 14:39:46 -0500

    indentation

--- pyincore/analyses/pipelinedamage/test_pipeline_dmg.py
+++ pyincore/analyses/pipelinedamage/test_pipeline_dmg.py
@@ -8,12 +8,13 @@ def test_pipeline_dmg():
 
     # test tsunami pipeline
     pipeline_dmg.load_remote_input_dataset("pipeline",
-                                               "5d2666b5b9219c3c5595ee65")
+                                           "5d2666b5b9219c3c5595ee65")
     pipeline_dmg.set_parameter("result_name",
-                                   "seaside_tsunami_pipeline_result")
+                               "seaside_tsunami_pipeline_result")
     pipeline_dmg.set_parameter("mapping_id", "5d320a87b9219c6d66398b45")
     pipeline_dmg.set_parameter("hazard_type", "tsunami")
-    pipeline_dmg.set_parameter("fragility_key", "Non-Retrofit inundationDepth Fragility ID Code")
+    pipeline_dmg.set_parameter("fragility_key",
+                               "Non-Retrofit inundationDepth Fragility ID Code")
     pipeline_dmg.set_parameter("hazard_id", "5bc9eaf7f7b08533c7e610e1")
     pipeline_dmg.set_parameter("num_cpu", 4)
 
