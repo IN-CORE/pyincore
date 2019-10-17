8dfb738508d6b8571079a9a81a037f82dd96638a
Kim <ywkim@illinois.edu>
2019-10-17 10:42:35 -0500

    added release script for pyincore documentation image

---
+++ docs/release.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+# Exit on error
+set -e
+
+# use DEBUG=echo ./release-all.sh to print all commands
+export DEBUG=${DEBUG:-""}
+
+$DEBUG docker login hub.ncsa.illinois.edu
+
+# Find out what branch we are on
+BRANCH=${BRANCH:-"$(git rev-parse --abbrev-ref HEAD)"}
+
+# Find out the version
+if [ "$BRANCH" = "master" ]; then
+    VERSION=""
+elif [ "${BRANCH}" = "develop" ]; then
+    VERSION="-dev"
+else
+    exit 0
+fi
+
+#TODO this should be replaced with tagging
+# Push pyincore documenation image
+docker push hub.ncsa.illinois.edu/incore/doc/pyincore$VERSION:latest
