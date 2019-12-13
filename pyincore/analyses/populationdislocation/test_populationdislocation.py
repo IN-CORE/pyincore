69a7e1a8addcc9835890e9200d0a82809608d63b
ncsa-mo <ondrejce@illinois.edu>
2019-12-13 17:33:31 -0600

    change test file

--- pyincore/analyses/populationdislocation/test_populationdislocation.py
+++ pyincore/analyses/populationdislocation/test_populationdislocation.py
@@ -16,8 +16,9 @@ def run_without_base_class():
         building_dmg_file='bldg_dmg_result.csv',
         housing_unit_allocation_file='housingunitallocation_1111.csv',
         block_data_file='bgdata.csv')
+    value_loss = 'value_loss.csv'
 
-    merged_final_inv = podi.get_dislocation(seed_i, merged_block_inv)
+    merged_final_inv = podi.get_dislocation(seed_i, merged_block_inv, value_loss)
 
     # save to csv
     merged_final_inv.to_csv("final_inventory" + str(seed_i) + ".csv", sep=",")
@@ -30,11 +31,13 @@ def run_with_base_class():
     building_dmg = "5c12856d1f5e0d0667d6410e"
     housing_unit_alloc = "5d543b06b9219c0689b987af"
     bg_data = "5d542bd8b9219c0689b90408"
+    value_loss = ""
 
     # Joplin
     #building_dmg = ""
     #housing_unit_alloc = ""
     #bg_data = "5d4c9545b9219c0689b2358a"
+    #value_loss = ""
 
 
     pop_dis = PopulationDislocation(client)
@@ -42,6 +45,7 @@ def run_with_base_class():
     pop_dis.load_remote_input_dataset("building_dmg", building_dmg)
     pop_dis.load_remote_input_dataset("housing_unit_allocation", housing_unit_alloc)
     pop_dis.load_remote_input_dataset("block_group_data", bg_data)
+    pop_dis.load_remote_input_dataset("value_poss_param", value_loss)
 
     # pop_dis.show_gdocstr_docs()
 
