c7a2fbc2132e04f39e2f36c14f0b4254190e2114
Diego Calderon <diegoac2@illinois.edu>
2019-12-12 22:58:30 -0600

    updated pyIncore client

--- pyincore/client.py
+++ pyincore/client.py
@@ -4,9 +4,12 @@
 # terms of the Mozilla Public License v2.0 which accompanies this distribution,
 # and is available at https://www.mozilla.org/en-US/MPL/2.0/
 
+from pathlib import Path
+
 import getpass
 import requests
 import pyincore.globals as pyglobals
+import os
 
 logger = pyglobals.LOGGER
 
@@ -15,176 +18,8 @@ class Client:
     """Incore service Client class. It handles connection to the server with INCORE services and user authentication."""
 
     def __init__(self):
-        pass
-
-    def make_request(self, func):
-        def request_wrapper(*args, **kwargs):
-            try:
-                return func(*args, **kwargs)
-            except requests.exceptions:
-                access_token = self.login()
-                self.session["Authorization"] = access_token
-                try:
-                    return func(*args, **kwargs)
-                except Exception as e:
-                    raise e
-            return request_wrapper
-
-    @make_request
-    def get(self, url: str, params=None, timeout=(30, 600), **kwargs):
-        """Get server connection response.
-
-        Args:
-            url (str): Service url.
-            params (obj): Session parameters.
-            timeout (int): Session timeout.
-            **kwargs: A dictionary of external parameters.
-
-        Returns:
-            obj: HTTP response.
-
-        """
-        r = None
-        try:
-            r = self.session.get(url, params=params, timeout=timeout, **kwargs)
-            r.raise_for_status()
-            return r
-        except requests.exceptions.HTTPError:
-            logger.error('HTTPError: The server returned a ' + str(r.status_code) + ' failure response code. You can '
-                         'find information about HTTP response status codes here: '
-                         'https://developer.mozilla.org/en-US/docs/Web/HTTP/Status')
-            raise
-        except requests.exceptions.ConnectionError:
-            logger.error("ConnectionError: Failed to establish a connection with the server. "
-                         "This might be due to a refused connection. Please check that you are using the right URLs.")
-            raise
-        except requests.exceptions.RequestException:
-            logger.error("RequestException: There was an exception while trying to handle your request. "
-                         "Please go to the end of this message for more specific information about the exception.")
-            raise
-
-    @make_request
-    def post(self, url: str, data=None, json=None, timeout=(30, 600), **kwargs):
-        """Post data on the server.
-
-        Args:
-            url (str): Service url.
-            data (obj): Data to be posted on the server.
-            json (obj): Description of the data, metadata json.
-            timeout (int): Session timeout.
-            **kwargs: A dictionary of external parameters.
-
-        Returns:
-            obj: HTTP response.
-
-        """
-        r = None
-        try:
-            r = self.session.post(url, data=data, json=json, timeout=timeout, **kwargs)
-            r.raise_for_status()
-            return r
-        except requests.exceptions.HTTPError:
-            logger.error('HTTPError: The server returned a ' + str(r.status_code) + ' failure response code. You can '
-                         'find information about HTTP response status codes here: '
-                         'https://developer.mozilla.org/en-US/docs/Web/HTTP/Status')
-            raise
-        except requests.exceptions.ConnectionError:
-            logger.error("ConnectionError: Failed to establish a connection with the server. "
-                         "This might be due to a refused connection. Please check that you are using the right URLs.")
-            raise
-        except requests.exceptions.RequestException:
-            logger.error("RequestException: There was an exception while trying to handle your request. "
-                         "Please go to the end of this message for more specific information about the exception.")
-            raise
-
-    @make_request
-    def put(self, url: str, data=None, timeout=(30, 600), **kwargs):
-        """Put data on the server.
-
-        Args:
-            url (str): Service url.
-            data (obj): Data to be put onn the server.
-            timeout (int): Session timeout.
-            **kwargs: A dictionary of external parameters.
-
-        Returns:
-            obj: HTTP response.
-
-        """
-        r = None
-        try:
-            r = self.session.put(url, data=data, timeout=timeout, **kwargs)
-            r.raise_for_status()
-            return r
-        except requests.exceptions.HTTPError:
-            logger.error('HTTPError: The server returned a ' + str(r.status_code) + ' failure response code. You can '
-                         'find information about HTTP response status codes here: '
-                         'https://developer.mozilla.org/en-US/docs/Web/HTTP/Status')
-            raise
-        except requests.exceptions.ConnectionError:
-            logger.error("ConnectionError: Failed to establish a connection with the server. "
-                         "This might be due to a refused connection. Please check that you are using the right URLs.")
-            raise
-        except requests.exceptions.RequestException:
-            logger.error("RequestException: There was an exception while trying to handle your request. "
-                         "Please go to the end of this message for more specific information about the exception.")
-            raise
-
-    @make_request
-    def delete(self, url: str, timeout=(30, 600), **kwargs):
-        """Delete data on the server.
-
-        Args:
-            url (str): Service url.
-            timeout (int): Session timeout.
-            **kwargs: A dictionary of external parameters.
-
-        Returns:
-            obj: HTTP response.
-
-        """
-        r = None
-        try:
-            r = self.session.delete(url, timeout=timeout, **kwargs)
-            r.raise_for_status()
-            return r
-        except requests.exceptions.HTTPError:
-            logger.error('HTTPError: The server returned a ' + str(r.status_code) + ' failure response code. You can '
-                         'find information about HTTP response status codes here: '
-                         'https://developer.mozilla.org/en-US/docs/Web/HTTP/Status')
-            raise
-        except requests.exceptions.ConnectionError:
-            logger.error("ConnectionError: Failed to establish a connection with the server. "
-                         "This might be due to a refused connection. Please check that you are using the right URLs.")
-            raise
-        except requests.exceptions.RequestException:
-            logger.error("RequestException: There was an exception while trying to handle your request. "
-                         "Please go to the end of this message for more specific information about the exception.")
-            raise
-
-
-class IncoreClient(Client):
-    """Incore service Client class. It contains token and service root url.
-
-    Args:
-        service_url (str): Service url.
-
-    """
-    def __init__(self, service_url: str = None, token_file: str = None) -> object:
-        if service_url is None or len(service_url.strip()) == 0:
-            service_url = pyglobals.INCORE_API_PROD_URL
-        self.service_url = service_url
-
-        r = self.retrieve_token_from_file(token_file)
-        if r is None:
-            r = self.login()
-            if r is None:
-                logger.warning("Authentication Failed. Please check the credentials and try again")
-                exit(0)
-            self.store_authorization_in_file(token_file, str("bearer" + r["access_token"]))
-
+        self.token_file = None
         self.session = requests.session()
-        self.session.headers['Authorization'] = r["Authorization"]
 
     @staticmethod
     def login():
@@ -201,60 +36,216 @@ class IncoreClient(Client):
 
         return None
 
-    @staticmethod
-    def store_authorization_in_file(token_file: str, authorization: str):
+    def store_authorization_in_file(self, authorization: str):
         """
         Store the access token in local file. If the file does not exist, this function creates it.
-        :param token_file: Name of file to store authorization
         :param authorization: authorization in the format "bearer access_token"
         :return: None
         """
-        if token_file is None:
-            token_file = pyglobals.TOKEN_FILE_NAME
-        print(authorization, token_file)
-        return None
+        try:
+            with open(self.token_file, 'w') as f:
+                f.write(authorization)
+                f.close()
+        except IOError as e:
+            logger.warning("Unable to write file. I/O error({0}): {1}".format(e.errno, e.strerror))
 
-    @staticmethod
-    def retrieve_token_from_file(token_file: str):
+    def retrieve_token_from_file(self):
         """
         Attempts to retrieve authorization from a local file, if it exists.
-        :param token_file: Name of file to retrieve authorization from
         :return: Dictionary containing authorization in  the format "bearer access_token" if file exists, None otherwise
         """
-        if token_file is None:
-            token_file = pyglobals.TOKEN_FILE_NAME
-            print(token_file)
-        if True:
-            return {'Authorization': 'bearer token'}
-        else:
+        if not self.token_file.is_file():
             return None
+        else:
+            try:
+                with open(self.token_file, 'r') as f:
+                    auth = f.read().splitlines()
+                    f.close()
+                return auth[0]
+            except IndexError as e:
+                logger.exception("Error reading authorization from token file. "
+                                 "Index error({0}): {1}".format(e.errno, e.strerror))
+                raise
+            except OSError as e:
+                logger.exception("Error attempting to open token file."
+                                 "OS error({0}): {1}".format(e.errno, e.strerror))
+                raise
 
-    def update_session_authorization(self, authorization: str):
+    def refresh_token(self):
         """
-        Update the header authorization
-        :param authorization: Authorization in the format "bearer access_token"
-        :return: None
+        Call login function and update session's headers
+        :return:
         """
-        self.session["Authorization"] = authorization
+        r = self.login()
+        if r is not None and r["access_token"] is not None:
+            authorization = str("bearer" + r["access_token"])
+            self.store_authorization_in_file(authorization)
+            self.session.headers["Authorization"] = authorization
+        else:
+            logger.warning("Authentication failed.")
+            exit(0)
+
+    @staticmethod
+    def make_request(func):
+        def request_wrapper(*args, **kwargs):
+            try:
+                r = func(*args, **kwargs)
+                r.raise_for_status()
+            except requests.exceptions.HTTPError:
+                logger.error('HTTPError: The server returned a '
+                             + str(r.status_code) + ' failure response code. You can '
+                             'find information about HTTP response status codes here: '
+                             'https://developer.mozilla.org/en-US/docs/Web/HTTP/Status')
+                raise
+            except requests.exceptions.ConnectionError:
+                logger.error("ConnectionError: Failed to establish a connection with the server. "
+                             "This might be due to a refused connection. "
+                             "Please check that you are using the right URLs.")
+                raise
+            except requests.exceptions.RequestException:
+                logger.error("RequestException: There was an exception while trying to handle your request. "
+                             "Please go to the end of this message for more specific information about the exception.")
+                raise
+
+            return request_wrapper
+
+    @make_request
+    def get(self, url: str, params=None, timeout=(30, 600), **kwargs):
+        """Get server connection response.
+
+        Args:
+            url (str): Service url.
+            params (obj): Session parameters.
+            timeout (int): Session timeout.
+            **kwargs: A dictionary of external parameters.
+
+        Returns:
+            obj: HTTP response.
+
+        """
+        r = self.session.get(url, params=params, timeout=timeout, **kwargs)
+
+        if r.status_code == 401:
+            self.refresh_token()
+            r = self.session.get(url, params=params, timeout=timeout, **kwargs)
+
+        return r
+
+    @make_request
+    def post(self, url: str, data=None, json=None, timeout=(30, 600), **kwargs):
+        """Post data on the server.
+
+        Args:
+            url (str): Service url.
+            data (obj): Data to be posted on the server.
+            json (obj): Description of the data, metadata json.
+            timeout (int): Session timeout.
+            **kwargs: A dictionary of external parameters.
+
+        Returns:
+            obj: HTTP response.
+
+        """
+        r = self.session.post(url, data=data, json=json, timeout=timeout, **kwargs)
+
+        if r.status_code == 401:
+            self.refresh_token()
+            r = self.session.post(url, data=data, json=json, timeout=timeout, **kwargs)
+
+        return r
+
+    @make_request
+    def put(self, url: str, data=None, timeout=(30, 600), **kwargs):
+        """Put data on the server.
+
+        Args:
+            url (str): Service url.
+            data (obj): Data to be put onn the server.
+            timeout (int): Session timeout.
+            **kwargs: A dictionary of external parameters.
+
+        Returns:
+            obj: HTTP response.
+
+        """
+        r = self.session.put(url, data=data, timeout=timeout, **kwargs)
+
+        if r.status_code == 401:
+            self.refresh_token()
+            r = self.session.put(url, data=data, timeout=timeout, **kwargs)
+
+        return r
+
+    @make_request
+    def delete(self, url: str, timeout=(30, 600), **kwargs):
+        """Delete data on the server.
+
+        Args:
+            url (str): Service url.
+            timeout (int): Session timeout.
+            **kwargs: A dictionary of external parameters.
+
+        Returns:
+            obj: HTTP response.
+
+        """
+        r = self.session.delete(url, timeout=timeout, **kwargs)
+
+        if r.status_code == 401:
+            self.refresh_token()
+            r = self.session.delete(url, timeout=timeout, **kwargs)
+
+        return r
+
+
+class IncoreClient(Client):
+    """IN-CORE secure service client class. It contains token and service root url.
+
+    Args:
+        service_url (str): Service url.
+
+    """
+    def __init__(self, service_url: str = None, token_file_name: str = None):
+        super().__init__()
+        if service_url is None or len(service_url.strip()) == 0:
+            service_url = pyglobals.INCORE_API_PROD_URL
+        self.service_url = service_url
+
+        if token_file_name is None or len(token_file_name.strip()) == 0:
+            token_file_name = pyglobals.TOKEN_FILE_NAME
+        self.token_file = Path(os.path.join(pyglobals.PYINCORE_USER_CACHE, token_file_name))
+
+        r = self.retrieve_token_from_file()
+        if r is None or r["access_token"] is None:
+            r = self.login()
+            if r is None or r["access_token"] is None:
+                logger.warning("Authentication Failed.")
+                exit(0)
+            self.store_authorization_in_file(token_file_name, str("bearer" + r["access_token"]))
+
+        self.session.headers['Authorization'] = r["Authorization"]
 
 
 class InsecureIncoreClient(Client):
-    """Incore service Client class. It contains token and service root url.
+    """IN-CORE insecure service client class.
 
     Args:
         service_url (str): Service url.
         user_info (str): Preferred username field from keycloak's user-info
 
     """
-    def __init__(self, service_url: str = None, user_info: str = None) -> object:
-
+    def __init__(self, service_url: str = None, user_info: str = None, token_file_name: str = None):
+        super().__init__()
         if service_url is None or len(service_url.strip()) == 0:
             service_url = pyglobals.INCORE_API_DEV_INSECURE_URL
         self.service_url = service_url
 
         if user_info is None or len(user_info.strip()) == 0:
             user_info = pyglobals.INCORE_LDAP_TEST_USER_INFO
+        self.user_info = user_info
 
-        self.session = requests.session()
+        if token_file_name is None or len(token_file_name.strip()) == 0:
+            token_file_name = pyglobals.TOKEN_FILE_NAME
+        self.token_file = token_file_name
+
         self.session.headers['x-auth-userinfo'] = str(user_info)
-
