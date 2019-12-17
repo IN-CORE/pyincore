37ada7cf34829dce9aba1cbf3780f500f1ac50cd
Diego Calderon <diegoac2@illinois.edu>
2019-12-17 10:17:39 -0600

    removed unused globals variables

--- pyincore/globals.py
+++ pyincore/globals.py
@@ -11,13 +11,8 @@ import os
 
 PACKAGE_VERSION = "0.5.2"
 
-# INCORE_API_PROD_URL = "https://incore2-services.ncsa.illinois.edu"
 INCORE_API_PROD_URL = "https://incore-dev-kube.ncsa.illinois.edu"
-INCORE_API_INSECURE_URL = "http://incore2-services.ncsa.illinois.edu:8888"
-# INCORE_API_DEV_INSECURE_URL = "http://incore2-services-dev.ncsa.illinois.edu:8888"
 INCORE_API_DEV_INSECURE_URL = "http://localhost:8080"
-INCORE_API_DEV_URL = "https://incore2-services-dev.ncsa.illinois.edu"
-INCORE_LDAP_TEST_USER = "incrtest"
 
 KEYCLOAK_TOKEN_URL = "https://incore-dev-kube.ncsa.illinois.edu/auth/realms/In-core/protocol/openid-connect/token"
 CLIENT_ID = "react-auth"
@@ -28,7 +23,7 @@ PYINCORE_ROOT_FOLDER = os.path.dirname(os.path.dirname(__file__))
 USER_HOME = os.path.expanduser('~')
 USER_CACHE_DIR = ".incore"
 PYINCORE_USER_CACHE = os.path.join(USER_HOME, USER_CACHE_DIR)
-CRED_FILE_NAME = ".incorepw"
+
 DATA_CACHE_FOLDER_NAME = "cache_data"
 PYINCORE_USER_DATA_CACHE = os.path.join(PYINCORE_USER_CACHE, DATA_CACHE_FOLDER_NAME)
 
