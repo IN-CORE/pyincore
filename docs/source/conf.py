4005cc13b5b5da591a60bb8bd8d41de39471d3ec
Jong Lee <jonglee1@illinois.edu>
2019-12-23 14:27:00 -0600

    updated version in docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -34,9 +34,9 @@ project = 'pyIncore'
 author = ''
 
 # The short X.Y version
-version = '0.5.3'
+version = '0.5.4'
 # The full version, including alpha/beta/rc tags
-release = '0.5.3'
+release = '0.5.4'
 
 
 # -- General configuration ---------------------------------------------------
