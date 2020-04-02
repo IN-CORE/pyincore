7f33607204c2cd7dccb4a4ccf900a85672a47792
Chen Wang <cwang138@illinois.edu>
2020-04-02 10:22:45 -0500

    remove old analysis code

--- pyincore/analyses/epfdamage/__init__.py
+++ pyincore/analyses/epfdamage/__init__.py
@@ -1,3 +1,2 @@
 from pyincore.analyses.epfdamage.epfdamage import EpfDamage
-from pyincore.analyses.epfdamage.epfdamage_old import EpfDamageOld
 from pyincore.analyses.epfdamage.epfutil import EpfUtil
