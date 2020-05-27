c1321717ac3e5a880b7c01abb42cabb27781ced2
Chen Wang <cwang138@illinois.edu>
2020-05-27 09:11:30 -0500

    format of the license

--- pyincore/models/parametricfragilitycurve.py
+++ pyincore/models/parametricfragilitycurve.py
@@ -4,6 +4,7 @@
 # terms of the Mozilla Public License v2.0 which accompanies this distribution,
 # and is available at https://www.mozilla.org/en-US/MPL/2.0/
 
+
 import math
 
 from pyincore.models.fragilitycurve import FragilityCurve
