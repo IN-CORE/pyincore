e3866697e4f321e0d1306cc2477ea37ce4342555
Chen Wang <cwang138@illinois.edu>
2020-06-15 17:12:18 -0500

    update the test to use dev

--- tests/pyincore/analyses/buildingdamage/test_buildingdamage.py
+++ tests/pyincore/analyses/buildingdamage/test_buildingdamage.py
@@ -1,9 +1,10 @@
 from pyincore import IncoreClient, FragilityService, MappingSet
 from pyincore.analyses.buildingdamage import BuildingDamage
+from pyincore.globals import INCORE_API_DEV_URL
 
 
 def run_with_base_class():
-    client = IncoreClient()
+    client = IncoreClient(INCORE_API_DEV_URL)
 
     # Memphis Earthquake damage
     # New madrid earthquake using Atkinson Boore 1995
@@ -34,27 +35,30 @@ def run_with_base_class():
     # Run Analysis
     bldg_dmg.run_analysis()
 
-    #TSUNAMI
+    # TSUNAMI
 
-    # hazard_type = "tsunami"
-    # hazard_id = "5bc9e25ef7b08533c7e610dc"
-    #
-    # # Seaside building dataset
-    # bldg_dataset_id = "5bcf2fcbf242fe047ce79dad"
-    #
-    # # Tsunami mapping
-    # mapping_id = "5b48fb1f337d4a478e7bd54d"
-    #
-    # # Run seaside tsunami building damage
-    # bldg_dmg = BuildingDamage(client)
-    # bldg_dmg.load_remote_input_dataset("buildings", bldg_dataset_id)
-    # result_name = "seaside_tsunami_dmg_result"
-    # bldg_dmg.set_parameter("result_name", result_name)
-    # bldg_dmg.set_parameter("mapping_id", mapping_id)
-    # bldg_dmg.set_parameter("hazard_type", hazard_type)
-    # bldg_dmg.set_parameter("hazard_id", hazard_id)
-    # bldg_dmg.set_parameter("num_cpu", 4)
-    # bldg_dmg.run_analysis()
+    hazard_type = "tsunami"
+    hazard_id = "5bc9e25ef7b08533c7e610dc"
+
+    # Seaside building dataset
+    bldg_dataset_id = "5bcf2fcbf242fe047ce79dad"
+
+    # Run seaside tsunami building damage
+    bldg_dmg = BuildingDamage(client)
+    bldg_dmg.load_remote_input_dataset("buildings", bldg_dataset_id)
+
+    # Tsunami mapping
+    mapping_id = "5b48fb1f337d4a478e7bd54d"
+    fragility_service = FragilityService(client)
+    mapping_set = MappingSet(fragility_service.get_mapping(mapping_id))
+    bldg_dmg.set_input_dataset('dfr3_mapping_set', mapping_set)
+
+    result_name = "seaside_tsunami_dmg_result"
+    bldg_dmg.set_parameter("result_name", result_name)
+    bldg_dmg.set_parameter("hazard_type", hazard_type)
+    bldg_dmg.set_parameter("hazard_id", hazard_id)
+    bldg_dmg.set_parameter("num_cpu", 4)
+    bldg_dmg.run_analysis()
 
 
 if __name__ == '__main__':
