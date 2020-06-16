40617bda8635f757f7a87c314c9b787579ed98e8
Chen Wang <cwang138@illinois.edu>
2020-06-16 16:04:59 -0500

    add pytest

--- pyincore/analyses/bridgedamage/bridgeutil.py
+++ pyincore/analyses/bridgedamage/bridgeutil.py
@@ -75,20 +75,3 @@ class BridgeUtil:
         return BridgeUtil.BRIDGE_FRAGILITY_KEYS[target_fragility_key.lower()][
             1] \
             if target_fragility_key.lower() in BridgeUtil.BRIDGE_FRAGILITY_KEYS else "none"
-
-    @staticmethod
-    def write_to_file(output, fieldname_list, output_file_name):
-        """Generates output csv file with header.
-
-        Args:
-            output (str): A content to be written to output.
-            fieldname_list (list): A list of header names.
-            output_file_name (str): Output file name.
-
-        """
-        # Write Output to csv
-        with open(output_file_name, 'w') as csv_file:
-            writer = csv.DictWriter(csv_file, dialect="unix",
-                                    fieldnames=fieldname_list)
-            writer.writeheader()
-            writer.writerows(output)
---
+++ tests/pyincore/analyses/bridgedamage/test_bridgeutil.py
@@ -0,0 +1,34 @@
+# Copyright (c) 2019 University of Illinois and others. All rights reserved.
+#
+# This program and the accompanying materials are made available under the
+# terms of the Mozilla Public License v2.0 which accompanies this distribution,
+# and is available at https://www.mozilla.org/en-US/MPL/2.0/
+
+from pyincore.analyses.bridgedamage import BridgeUtil
+
+
+def test_bridge_damage_util():
+    assert BridgeUtil.BRIDGE_FRAGILITY_KEYS == {
+        "elastomeric bearing retrofit fragility id code": [
+            "Elastomeric Bearing", "eb"],
+        "steel jacket retrofit fragility id code": ["Steel Jacket", "sj"],
+        "restrainer cables retrofit fragility id code": ["Restrainer Cables",
+                                                         "rc"],
+        "seat extender retrofit fragility id code": ["Seat Extender", "se"],
+        "shear key retrofit fragility id code": ["Shear Key", "sk"],
+        "non-retrofit fragility id code": ["as built", "none"],
+        "non-retrofit inundationdepth fragility id code": ["as built", "none"]
+    }
+
+    assert BridgeUtil.DEFAULT_FRAGILITY_KEY == "Non-Retrofit Fragility ID Code"
+    assert BridgeUtil.DEFAULT_TSUNAMI_HMAX_FRAGILITY_KEY == "Non-Retrofit inundationDepth Fragility ID Code"
+
+
+def test_get_retrofit_cost():
+    assert BridgeUtil.get_retrofit_cost(BridgeUtil.DEFAULT_FRAGILITY_KEY) == 0.0
+
+def test_get_retrofit_type():
+    assert BridgeUtil.get_retrofit_type(BridgeUtil.DEFAULT_FRAGILITY_KEY) == "as built"
+
+def test_get_retrofit_code():
+    assert BridgeUtil.get_retrofit_code(BridgeUtil.DEFAULT_FRAGILITY_KEY) == "none"
