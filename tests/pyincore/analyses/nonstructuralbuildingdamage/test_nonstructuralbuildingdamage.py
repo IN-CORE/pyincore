193c6570cb2da025ccb753be55c3172d00bb1115
Chen Wang <cwang138@illinois.edu>
2020-06-19 10:35:43 -0500

    move test to dev and test folder

--- tests/pyincore/analyses/nonstructuralbuildingdamage/test_nonstructuralbuildingdamage.py
+++ tests/pyincore/analyses/nonstructuralbuildingdamage/test_nonstructuralbuildingdamage.py
@@ -1,10 +1,10 @@
-
 from pyincore import IncoreClient, FragilityService, MappingSet
 from pyincore.analyses.nonstructbuildingdamage import NonStructBuildingDamage
+from pyincore.globals import INCORE_API_DEV_URL
 
 
 def run_with_base_class():
-    client = IncoreClient()
+    client = IncoreClient(INCORE_API_DEV_URL)
 
     # Memphis 7.9 AB-95
     hazard_type = "earthquake"
