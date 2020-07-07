18dcb8d5de912585b8c915d65b8d9bc2a25a3f05
Chen Wang <cwang138@illinois.edu>
2020-07-07 16:07:35 -0500

    move test

---
+++ tests/pyincore/analyses/montecarlofailureprobability/test_montecarlofailureprobability.py
@@ -0,0 +1,27 @@
+from pyincore.client import IncoreClient
+from pyincore.analyses.montecarlofailureprobability import \
+    MonteCarloFailureProbability
+from pyincore.globals import INCORE_API_DEV_URL
+
+
+def run_with_base_class():
+    client = IncoreClient(INCORE_API_DEV_URL)
+    mc = MonteCarloFailureProbability(client)
+
+    # # example of using local dataset
+    # damage_dataset = Dataset.from_file("memphis_bldg_dmg_result.csv",
+    #                                    "ergo:buildingDamageVer4")
+    # mc.set_input_dataset("damage", damage_dataset)
+    mc.load_remote_input_dataset("damage", "5a29782fc7d30d4af537ace5")
+    mc.set_parameter("result_name", "mc_failure_probability")
+    mc.set_parameter("num_cpu", 8)
+    mc.set_parameter("num_samples", 10)
+    mc.set_parameter("damage_interval_keys",
+                     ["insignific", "moderate", "heavy", "complete"])
+    mc.set_parameter("failure_state_keys", ["moderate", "heavy", "complete"])
+
+    mc.run_analysis()
+
+
+if __name__ == '__main__':
+    run_with_base_class()
