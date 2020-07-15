5af2ca1b2b6fc5920e5ced910f5a3e1603206659
Chen Wang <cwang138@illinois.edu>
2020-07-15 16:25:55 -0500

    point test to dev; use remote ids

---
+++ tests/pyincore/analyses/buildingfunctionality/test_buildingfunctionality.py
@@ -0,0 +1,27 @@
+import time
+
+from pyincore import IncoreClient
+from pyincore.analyses.buildingfunctionality.buildingfunctionality import BuildingFunctionality
+from pyincore.globals import INCORE_API_DEV_URL
+
+
+def run_with_base_class():
+    client = IncoreClient(INCORE_API_DEV_URL)
+    bldg_func = BuildingFunctionality(client)
+
+    # load datasets remotely
+    bldg_func.load_remote_input_dataset("building_damage_mcs_samples", "5f0f6fbfb922f96f4e989ed8")
+    bldg_func.load_remote_input_dataset("substations_damage_mcs_samples", "5f0f71bab922f96f4e9a7511")
+    bldg_func.load_remote_input_dataset("poles_damage_mcs_samples", "5f0f7231b922f96f4e9a7538")
+    bldg_func.load_remote_input_dataset("interdependency_dictionary", "5f0f7311feef2d758c47cfab")
+
+    bldg_func.set_parameter("result_name", "Joplin_mcs_functionality_probability")
+    bldg_func.set_parameter("num_samples", 10)
+
+    start_time = time.time()
+    bldg_func.run_analysis()
+    print("--- %s seconds ---" % (time.time() - start_time))
+
+
+if __name__ == '__main__':
+    run_with_base_class()
