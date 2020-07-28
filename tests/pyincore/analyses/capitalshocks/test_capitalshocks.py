59c5dfd514518eab65b5102a268948d24214baa6
Diego Calderon <diegoac2@illinois.edu>
2020-07-28 16:50:17 -0500

    updated test

--- tests/pyincore/analyses/capitalshocks/test_capitalshocks.py
+++ tests/pyincore/analyses/capitalshocks/test_capitalshocks.py
@@ -4,17 +4,17 @@ from pyincore.analyses.capitalshocks import CapitalShocks
 
 def run_base_analysis():
     client = IncoreClient()
-    joplin_capital_shocks = CapitalShocks(client)
+    capital_shocks = CapitalShocks(client)
 
     # Building inventory
     building_inventory = "5dbc8478b9219c06dd242c0d"
     building_to_sectors = "5f202d674620b643d787a5e7"
     failure_probability = "5f20347933b2700c110a3dd2"
 
-    joplin_capital_shocks.load_remote_input_dataset("buildings", building_inventory)
-    joplin_capital_shocks.load_remote_input_dataset("buildings_to_sectors", building_to_sectors)
-    joplin_capital_shocks.load_remote_input_dataset("failure_probability", failure_probability)
-    joplin_capital_shocks.run_analysis()
+    capital_shocks.load_remote_input_dataset("buildings", building_inventory)
+    capital_shocks.load_remote_input_dataset("buildings_to_sectors", building_to_sectors)
+    capital_shocks.load_remote_input_dataset("failure_probability", failure_probability)
+    capital_shocks.run_analysis()
 
 
 if __name__ == '__main__':
