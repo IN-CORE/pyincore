e8526641981d6b73a862f567817a00b230adc008
Diego Calderon <diegoac2@illinois.edu>
2020-07-28 17:57:43 -0500

    added joplin test to style conformance test

--- tests/test_format.py
+++ tests/test_format.py
@@ -35,7 +35,8 @@ paths = [
     os.path.join(PYINCORE_ROOT_FOLDER, 'tests/pyincore/analyses/montecarlofailureprobability/'),
     os.path.join(PYINCORE_ROOT_FOLDER, 'pyincore/dataset.py'),
     os.path.join(PYINCORE_ROOT_FOLDER, 'pyincore/hazardservice.py'),
-    os.path.join(PYINCORE_ROOT_FOLDER, 'tests/pyincore/test_hazardservice.py')
+    os.path.join(PYINCORE_ROOT_FOLDER, 'tests/pyincore/test_hazardservice.py'),
+    os.path.join(PYINCORE_ROOT_FOLDER, 'tests/pyincore/analyses/joplincgeanalysis/test_joplincgeanalysis.py')
 ]
 
 
