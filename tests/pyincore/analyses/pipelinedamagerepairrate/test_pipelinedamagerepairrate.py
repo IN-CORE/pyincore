dca51188534d3005d69cd1cc0ea283355ee8c626
Diego Calderon <diegoac2@illinois.edu>
2020-09-17 14:16:43 -0500

    added token file reference to clients

--- tests/pyincore/analyses/pipelinedamagerepairrate/test_pipelinedamagerepairrate.py
+++ tests/pyincore/analyses/pipelinedamagerepairrate/test_pipelinedamagerepairrate.py
@@ -9,7 +9,7 @@ from pyincore.globals import INCORE_API_DEV_URL
 
 
 def test_pipeline_dmg_w_repair_rate():
-    client = IncoreClient(INCORE_API_DEV_URL)
+    client = IncoreClient(INCORE_API_DEV_URL, token_file_name=".incrtesttoken")
 
     # This is the Memphis Water Buried Pipeline with Topology dataset the Ergo repository
     pipeline_dataset_id = "5a284f28c7d30d13bc081d14"
