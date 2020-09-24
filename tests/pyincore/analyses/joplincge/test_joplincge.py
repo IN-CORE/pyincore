8109d090b24cba7ed87631c5063c9929e6f3d62e
Diego Calderon <diegoac2@illinois.edu>
2020-09-24 11:12:21 -0500

    fixed pep8 issues

--- tests/pyincore/analyses/joplincge/test_joplincge.py
+++ tests/pyincore/analyses/joplincge/test_joplincge.py
@@ -8,36 +8,36 @@ def run_base_analysis():
     joplin_cge = JoplinCGEModel(client)
 
     # SAM
-    SAM = "5cdc7b585648c4048fb53062"
+    sam = "5cdc7b585648c4048fb53062"
 
     # CAPITAL COMP
-    BB = "5cdc7d295648c4048fb53089"
+    bb = "5cdc7d295648c4048fb53089"
 
     # MISC TABLES
-    IOUT = "5cdc7e965648c4048fb530ef"
-    MISC = "5cdc7f4f5648c4048fb53150"
-    MISCH = "5cdc7fa05648c4048fb53172"
-    LANDCAP = "5cdc7f0a5648c4048fb5312e"
-    EMPLOY = "5cdc7df65648c4048fb530ab"
-    IGTD = "5cdc7e405648c4048fb530cd"
-    TAUFF = "5cdc81da5648c4048fb532b7"
-    JOBCR = "5cdc7ed25648c4048fb5310c"
-    OUTCR = "5cdc7fde5648c4048fb53194"
+    iout = "5cdc7e965648c4048fb530ef"
+    misc = "5cdc7f4f5648c4048fb53150"
+    misch = "5cdc7fa05648c4048fb53172"
+    landcap = "5cdc7f0a5648c4048fb5312e"
+    employ = "5cdc7df65648c4048fb530ab"
+    igtd = "5cdc7e405648c4048fb530cd"
+    tauff = "5cdc81da5648c4048fb532b7"
+    jobcr = "5cdc7ed25648c4048fb5310c"
+    outcr = "5cdc7fde5648c4048fb53194"
     sector_shocks = "5f20653e7887544479c6b94a"
 
     joplin_cge.set_parameter("model_iterations", 1)
 
-    joplin_cge.load_remote_input_dataset("SAM", SAM)
-    joplin_cge.load_remote_input_dataset("BB", BB)
-    joplin_cge.load_remote_input_dataset("IOUT", IOUT)
-    joplin_cge.load_remote_input_dataset("MISC", MISC)
-    joplin_cge.load_remote_input_dataset("MISCH", MISCH)
-    joplin_cge.load_remote_input_dataset("LANDCAP", LANDCAP)
-    joplin_cge.load_remote_input_dataset("EMPLOY", EMPLOY)
-    joplin_cge.load_remote_input_dataset("IGTD", IGTD)
-    joplin_cge.load_remote_input_dataset("TAUFF", TAUFF)
-    joplin_cge.load_remote_input_dataset("JOBCR", JOBCR)
-    joplin_cge.load_remote_input_dataset("OUTCR", OUTCR)
+    joplin_cge.load_remote_input_dataset("SAM", sam)
+    joplin_cge.load_remote_input_dataset("BB", bb)
+    joplin_cge.load_remote_input_dataset("IOUT", iout)
+    joplin_cge.load_remote_input_dataset("MISC", misc)
+    joplin_cge.load_remote_input_dataset("MISCH", misch)
+    joplin_cge.load_remote_input_dataset("LANDCAP", landcap)
+    joplin_cge.load_remote_input_dataset("EMPLOY", employ)
+    joplin_cge.load_remote_input_dataset("IGTD", igtd)
+    joplin_cge.load_remote_input_dataset("TAUFF", tauff)
+    joplin_cge.load_remote_input_dataset("JOBCR", jobcr)
+    joplin_cge.load_remote_input_dataset("OUTCR", outcr)
     joplin_cge.load_remote_input_dataset("sector_shocks", sector_shocks)
 
     joplin_cge.run_analysis()
