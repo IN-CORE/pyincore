d7d97a3ccdb1a5bef70e1937a9aa0276d3cc9201
Jong Lee <jonglee1@illinois.edu>
2021-02-18 11:34:11 -0600

    using system tmp folder

--- pyincore/analyses/joplincge/joplincge.py
+++ pyincore/analyses/joplincge/joplincge.py
@@ -7,6 +7,7 @@ from pyomo.environ import *
 from pyomo.opt import SolverFactory
 
 import os
+import tempfile
 import pandas as pd
 
 
@@ -1656,8 +1657,7 @@ class JoplinCGEModel(BaseAnalysis):
             with open(filename, 'a') as f:
                 f.write('model.obj = Objective(expr=-1*model.x' + str(obj) + ')')
 
-        def run_solver(cons_filename, temp_file_name=os.path.join(os.path.dirname(os.path.realpath(__file__)),
-                                                                  "solverconstants/tmp.py")):
+        def run_solver(cons_filename, temp_file_name):
             solver = 'ipopt'
             solver_io = 'nl'
             stream_solver = True  # True prints solver output to screen
@@ -1742,8 +1742,12 @@ class JoplinCGEModel(BaseAnalysis):
         '''
 
         soln = []
-        filename = os.path.join(os.path.dirname(os.path.realpath(__file__)), "solverconstants/ipopt_cons.py")
-        tmp = os.path.join(os.path.dirname(os.path.realpath(__file__)), "solverconstants/tmp.py")
+
+        # create CGE tmp folder, solverconstatns
+        cge_tmp_folder = os.path.join(tempfile.gettempdir(), "solverconstants")
+        os.mkdir(cge_tmp_folder)
+        filename = os.path.join(cge_tmp_folder, "ipopt_cons.py")
+        tmp = os.path.join(cge_tmp_folder, "tmp.py")
         # print("Calibration: ")
         run_solver(filename, tmp)
 
