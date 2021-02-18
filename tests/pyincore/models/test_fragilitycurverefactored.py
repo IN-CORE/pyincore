27569345bf9265508b9d307c53d88a9934b71019
vnarah2 <vnarah2@illinois.edu>
2021-02-18 13:11:47 -0600

    INCORE1-696: Remove a hardcode in pytest

--- tests/pyincore/models/test_fragilitycurverefactored.py
+++ tests/pyincore/models/test_fragilitycurverefactored.py
@@ -36,9 +36,9 @@ def test_calculate_limit_state_probability(hazard_values, expected):
     ("fragility_curves/ParametricFragilityCurve_original.json", 4,
      "fragility_curves/ParametricFragilityCurve_refactored.json", {"PGA": 4}, 1),
     ("fragility_curves/PeriodBuildingFragilityCurve_original.json", 0.05,
-     "fragility_curves/PeriodBuildingFragilityCurve_refactored.json", {"Sa": 0.05}, 1),
+     "fragility_curves/PeriodBuildingFragilityCurve_refactored.json", {"Sa": 0.05}, 6),
     ("fragility_curves/PeriodStandardFragilityCurve_original.json", 4,
-     "fragility_curves/PeriodStandardFragilityCurve_refactored.json", {"0.2 sec Sa": 4}, 1),
+     "fragility_curves/PeriodStandardFragilityCurve_refactored.json", {"0.2 sec Sa": 4}, 6),
     ("fragility_curves/StandardFragilityCurve_original.json", 4,
      "fragility_curves/StandardFragilityCurve_refactored.json", {"momentumFlux": 4}, 1),
 ])
@@ -47,13 +47,13 @@ def test_curves_results(curve, hazard_val, refactored_curve, hazard_val_refactor
     refactored_fragility_set = get_fragility_set(refactored_curve)
 
     # add period if applicable
-    num_stories = 1
+
     building_period = fragility_set.fragility_curves[0].get_building_period(num_stories)
     if len(fragility_set.fragility_curves) <= 3:
         result = fragility_set.calculate_limit_state_w_conversion(hazard_val, period=building_period)
         refactored_result = refactored_fragility_set.calculate_limit_state_refactored_w_conversion(
             hazard_val_refactored,
-            num_stories=1)
+            num_stories=num_stories)
 
         assert result == refactored_result
 
