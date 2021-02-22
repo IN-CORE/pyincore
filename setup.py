8a228e95e77fca289b8ac6a840f133f269a926a5
Yong Wook Kim <ywkim@illinois.edu>
2021-02-22 16:08:26 -0600

    release version 0.9.0

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pyincore',
-    version='0.8.1',
+    version='0.9.0',
     packages=find_packages(where=".", exclude=["*.tests", "*.tests.*", "tests.*", "tests"]),
     include_package_data=True,
     package_data={
