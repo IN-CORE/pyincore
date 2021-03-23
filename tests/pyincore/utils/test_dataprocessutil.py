336663939edb29f1b9967e3b5bdfa5d093c77465
Chen Wang <cwang138@illinois.edu>
2021-03-23 18:11:47 -0500

    uncomment damage output with new ds names

--- tests/pyincore/utils/test_dataprocessutil.py
+++ tests/pyincore/utils/test_dataprocessutil.py
@@ -11,8 +11,8 @@ def test_get_mapped_result_from_dataset_id():
     client = IncoreClient(INCORE_API_DEV_URL)
 
     bldg_dataset_id = "5f9091df3e86721ed82f701d"
-    bldg_dmg_dataset_id = "5f9868c00ace240b22a7f2a5" # legacy DS_name
-    # bldg_dmg_dataset_id = "602d96e4b1db9c28aeeebdce" # new DS_name
+    # bldg_dmg_dataset_id = "5f9868c00ace240b22a7f2a5" # legacy DS_name
+    bldg_dmg_dataset_id = "602d96e4b1db9c28aeeebdce" # new DS_name
     archetype_id = "5fca915fb34b193f7a44059b"
 
     ret_json, mapped_df = util.get_mapped_result_from_dataset_id(
