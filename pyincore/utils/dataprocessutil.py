afc2dc65a84f6371160277ecf8deb27eb489c257
Yong Wook Kim <ywkim@illinois.edu>
2021-03-24 10:35:24 -0500

    added joining method

--- pyincore/utils/dataprocessutil.py
+++ pyincore/utils/dataprocessutil.py
@@ -145,3 +145,57 @@ class DataProcessUtil:
         ret_json = json.dumps({"by_cluster": json_by_cluster, "by_category": json_by_category})
 
         return ret_json, mapped_df
+
+    @staticmethod
+    def join_datasets(geodataset, tabledataset):
+        """Join Geopands geodataframe and non-geospatial Dataset using GUID field
+
+        Args:
+            geodataset (Dataset):  pyincore Dataset object with geospatial data
+            tabledataset (Dataset): pyincore Dataset object without geospatial data
+
+        Returns:
+            GeoDataFrame: Geopandas DataFrame object
+
+        """
+        gdf = gpd.read_file(geodataset.local_file_path)
+        df = tabledataset.get_dataframe_from_csv()
+        join_gdf = gdf.set_index("guid").join(df.set_index("guid"))
+
+        return join_gdf
+
+    @staticmethod
+    def join_table_dataset_with_source_dataset(dataset, client):
+        """Creates geopandas geodataframe by joining table dataset and its source dataset
+
+            Args:
+                dataset (Dataset): pyincore dataset object
+                client (Client): pyincore service client object
+
+            Returns:
+                joined_dbf (geodataframe): geopandas geodataframe object
+
+        """
+        is_source_dataset = False
+        source_dataset = None
+
+        # check if the given dataset is table dataset
+        if dataset.metadata['format'] != 'table' and dataset.metadata['format'] != 'csv':
+            print("The given dataset is not a table dataset")
+            return None
+
+        # check if source dataset exists
+        try:
+            source_dataset = dataset.metadata['sourceDataset']
+            is_source_dataset = True
+        except Exception:
+            print("There is no source dataset for the give table dataset")
+
+        if is_source_dataset:
+            # merge dataset and source dataset
+            geodataset = Dataset.from_data_service(source_dataset, DataService(client))
+            joined_gdf = DataProcessUtil.join_datasets(geodataset, dataset)
+        else:
+            return None
+
+        return joined_gdf
