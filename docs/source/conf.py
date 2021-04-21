0a501404ea09a2ecb498598571044d8cf9c7a568
MO <ondrejce@illinois.edu>
2021-04-21 15:18:37 -0500

    changelog and doc version

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -33,9 +33,9 @@ project = 'pyIncore'
 author = ''
 
 # The short X.Y version
-version = '0.9.1'
+version = '0.9.2'
 # The full version, including alpha/beta/rc tags
-release = '0.9.1'
+release = '0.9.2'
 
 
 # -- General configuration ---------------------------------------------------
