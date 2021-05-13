f52e418113ac5d40a1e7d7d0bdfb857ad636140f
Chen Wang <cwang138@illinois.edu>
2021-05-13 16:12:37 -0500

    bump the version up

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -33,9 +33,9 @@ project = 'pyIncore'
 author = ''
 
 # The short X.Y version
-version = '0.9.2'
+version = '0.9.3'
 # The full version, including alpha/beta/rc tags
-release = '0.9.2'
+release = '0.9.3'
 
 
 # -- General configuration ---------------------------------------------------
--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pyincore',
-    version='0.9.2',
+    version='0.9.3',
     packages=find_packages(where=".", exclude=["*.tests", "*.tests.*", "tests.*", "tests"]),
     include_package_data=True,
     package_data={
