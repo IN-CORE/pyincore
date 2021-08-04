520cb37b7435c62c80510b787be1bb1638094474
Diego Calderon <diegoac2@illinois.edu>
2021-08-04 11:32:55 -0500

    updated versions

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -33,9 +33,9 @@ project = 'pyIncore'
 author = ''
 
 # The short X.Y version
-version = '0.9.5'
+version = '0.9.6'
 # The full version, including alpha/beta/rc tags
-release = '0.9.5'
+release = '0.9.6.rc'
 
 
 # -- General configuration ---------------------------------------------------
--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pyincore',
-    version='0.9.5',
+    version='0.9.6',
     packages=find_packages(where=".", exclude=["*.tests", "*.tests.*", "tests.*", "tests"]),
     include_package_data=True,
     package_data={
