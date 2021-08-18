8bcfb60cdadeae02ce61c2ecac47dc70bc758b07
Yong Wook Kim <ywkim@illinois.edu>
2021-08-18 11:56:42 -0500

    automatic docker build and publish

--- docs/docker.sh
+++ docs/docker.sh
@@ -9,17 +9,8 @@ export DEBUG=${DEBUG:-""}
 # Find out what branch we are on
 BRANCH=${BRANCH:-"$(git rev-parse --abbrev-ref HEAD)"}
 
-# Find out the version
-if [ "$BRANCH" = "master" ]; then
-    VERSION=""
-elif [ "${BRANCH}" = "develop" ]; then
-    VERSION="-dev"
-else
-    exit 0
-fi
-
 # go to parent directory to create a docker
 cd ../
 
 # Build docker image
-$DEBUG docker build -t hub.ncsa.illinois.edu/incore/doc/pyincore$VERSION:latest -f Dockerfile.docs .
+$DEBUG docker build -t incore/doc/pyincore -f Dockerfile.docs .
