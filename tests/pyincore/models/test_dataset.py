ce2ff21f1e2f3c2a009be1930634ad4b623c294f
Chen Wang <cwang138@illinois.edu>
2021-10-18 13:49:57 -0500

    add test

---
+++ tests/pyincore/models/test_dataset.py
@@ -0,0 +1,23 @@
+import pandas as pd
+
+from pyincore.dataset import Dataset
+
+
+def test_from_dataframe():
+    df = pd.DataFrame()
+    dataset = Dataset.from_csv_data(df, "empty.csv", "ergo:buildingDamageVer6")
+    assert dataset.data_type == "ergo:buildingDamageVer6"
+
+
+def test_from_csv_data():
+    result_data = []
+    dataset = Dataset.from_csv_data(result_data, "empty.csv", "ergo:buildingDamageVer6")
+    assert dataset.data_type == "ergo:buildingDamageVer6"
+
+
+def test_from_json_data():
+    result_data = {}
+    dataset = Dataset.from_json_data(result_data, "empty.json", "incore:buildingDamageSupplement")
+    assert dataset.data_type == "incore:buildingDamageSupplement"
+
+
