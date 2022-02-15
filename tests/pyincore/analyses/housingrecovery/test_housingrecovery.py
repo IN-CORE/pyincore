efb1b61393fa40079de6150637c84857ceab21ed
ncsa-mo <ondrejce@illinois.edu>
2022-02-15 14:59:49 -0600

    add Galveston chained analyses in test script

--- tests/pyincore/analyses/housingrecovery/test_housingrecovery.py
+++ tests/pyincore/analyses/housingrecovery/test_housingrecovery.py
@@ -1,4 +1,8 @@
 from pyincore import IncoreClient
+from pyincore import MappingSet, FragilityService
+from pyincore.analyses.buildingdamage.buildingdamage import BuildingDamage
+from pyincore.analyses.housingunitallocation import HousingUnitAllocation
+from pyincore.analyses.populationdislocation import PopulationDislocation
 from pyincore.analyses.housingrecovery.housingrecovery import HousingRecovery
 import pyincore.globals as pyglobals
 
@@ -6,16 +10,133 @@ import pyincore.globals as pyglobals
 def run_with_base_class():
     client = IncoreClient(pyglobals.INCORE_API_DEV_URL)
 
-    # Building damage, Galveston, TX
-    bldg_damage_id = ""
+    # *************************************************************
+    # Set up and run Building damage, Galveston, TX analysis
+    # *************************************************************
+    hazard_type = "hurricane"
+    # Galveston deterministic Hurricane, 3 datasets - Kriging
+    hazard_id = "5fa5a228b6429615aeea4410"
+
+    # Galveston, TX Building inventory
+    bldg_inv_id = "60354b6c123b4036e6837ef7"  # 19k buildings with age_group
+
+    # Hurricane building mapping (with equation)
+    mapping_id = "602c381a1d85547cdc9f0675"
+    fragility_service = FragilityService(client)
+    mapping_set = MappingSet(fragility_service.get_mapping(mapping_id))
+
+    bldg_dmg = BuildingDamage(client)
+    bldg_dmg.load_remote_input_dataset("buildings", bldg_inv_id)
+    bldg_dmg.set_input_dataset("dfr3_mapping_set", mapping_set)
+
+    result_name = "Galveston_bldg_dmg_result"
+
+    bldg_dmg.set_parameter("fragility_key", "Hurricane SurgeLevel and WaveHeight Fragility ID Code")
+    bldg_dmg.set_parameter("result_name", result_name)
+    bldg_dmg.set_parameter("hazard_type", hazard_type)
+    bldg_dmg.set_parameter("hazard_id", hazard_id)
+    bldg_dmg.set_parameter("num_cpu", 4)
+
+    # Run Building damage
+    bldg_dmg.run_analysis()
+
+    # Retrieve result dataset
+    building_dmg_result = bldg_dmg.get_output_dataset("ds_result")
+
+    # Convert dataset to Pandas DataFrame
+    bdmg_df = building_dmg_result.get_dataframe_from_csv(low_memory=False)
+
+    # Display top 5 rows of output data
+    bdmg_df.head()
+
+    # *************************************************************
+    # Set up and run Housing Unit Allocation (HUA) analysis
+    # *************************************************************
+    # Housing unit inventory
+    housing_unit_inv = "5fc6ab1cd2066956f49e7a03"
+    # Address point inventory
+    address_point_inv = "5fc6aadcc38a0722f563392e"
+
+    # Create housing allocation
+    hua = HousingUnitAllocation(client)
+
+    # Load input dataset
+    hua.load_remote_input_dataset("housing_unit_inventory", housing_unit_inv)
+    hua.load_remote_input_dataset("address_point_inventory", address_point_inv)
+    hua.load_remote_input_dataset("buildings", bldg_inv_id)
+
+    # Specify the result name
+    result_name = "Galveston_HUA"
+
+    seed = 1238
+    iterations = 1
+
+    # Set analysis parameters
+    hua.set_parameter("result_name", result_name)
+    hua.set_parameter("seed", seed)
+    hua.set_parameter("iterations", iterations)
+
+    # Run Housing unit allocation analysis
+    hua.run_analysis()
+
+    # Retrieve result dataset
+    hua_result = hua.get_output_dataset("result")
+
+    # Convert dataset to Pandas DataFrame
+    hua_df = hua_result.get_dataframe_from_csv(low_memory=False)
+
+    # Display top 5 rows of output data
+    hua_df[['guid', 'numprec', 'incomegroup']].head()
+
+    # *************************************************************
+    # Set up and run Population Dislocation analysis
+    # *************************************************************
+
+    # Block group data, IN-CORE_BGMAP_2021-01-19_GalvestonTX
+    bg_data = "603545f2dcda03378087e708"
+    # Value loss parameters DS 0-3
+    value_loss = "60354810e379f22e16560dbd"
+
+    pop_dis = PopulationDislocation(client)
+
+    pop_dis.load_remote_input_dataset("block_group_data", bg_data)
+    pop_dis.load_remote_input_dataset("value_poss_param", value_loss)
+
+    pop_dis.set_input_dataset("building_dmg", building_dmg_result)
+    pop_dis.set_input_dataset("housing_unit_allocation", hua_result)
+
+    result_name = "galveston-pop-disl-results"
+    seed = 1111
+
+    pop_dis.set_parameter("result_name", result_name)
+    pop_dis.set_parameter("seed", seed)
+
+    pop_dis.run_analysis()
+
+    # Retrieve result dataset
+    pd_result = pop_dis.get_output_dataset("result")
+
+    # Convert dataset to Pandas DataFrame
+    pd_df = pd_result.get_dataframe_from_csv(low_memory=False)
+
+    # Display top 5 rows of output data
+    pd_df.head()
+
+    # *************************************************************
+    # Set up and run Housing Recovery analysis
+    # *************************************************************
+
+    # bldg_damage_id = ""
     # Population dislocation
-    pop_disl_id = ""
+    # pop_disl_id = ""
     # Census appraisal file
     census_appr_id = ""
 
     housing_rec = HousingRecovery(client)
-    housing_rec.load_remote_input_dataset("building_damage", bldg_damage_id)
-    housing_rec.load_remote_input_dataset("population_dislocation", pop_disl_id)
+    # housing_rec.load_remote_input_dataset("building_damage", bldg_damage_id)
+    housing_rec.set_input_dataset("building_dmg", building_dmg_result)
+    # housing_rec.load_remote_input_dataset("population_dislocation", pop_disl_id)
+    housing_rec.set_input_dataset("population_dislocation", pd_result)
     housing_rec.load_remote_input_dataset("census_appraisal_data", census_appr_id)
 
     result_name = "building_values"
