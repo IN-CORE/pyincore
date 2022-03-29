c6381a06c99863eb10825f418b4b2fc0017381ff
Diego Calderon <diegoac2@illinois.edu>
2022-03-29 14:18:05 -0500

    updated version

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -33,9 +33,9 @@ project = 'pyIncore'
 author = ''
 
 # The short X.Y version
-version = '1.3'
+version = '1.4'
 # The full version, including alpha/beta/rc tags
-release = '1.3.0'
+release = '1.4.0'
 
 # -- General configuration ---------------------------------------------------
 
