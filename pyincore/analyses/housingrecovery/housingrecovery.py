d742bf26c3edc0414e9bb78c8e7125b4b74b0809
Santiago Nunez-Corrales <nunezco2@illinois.edu>
2022-04-22 13:04:29 -0500

    Set up result_csv_data correctly.

--- pyincore/analyses/housingrecovery/housingrecovery.py
+++ pyincore/analyses/housingrecovery/housingrecovery.py
@@ -176,12 +176,9 @@ class HousingRecovery(BaseAnalysis):
         hse_rec_fin2 = pd.DataFrame(hse_rec_fin_index, columns=index_yr)
         hse_recov = pd.concat([hse_recov, hse_rec_fin1, hse_rec_fin2], axis=1)
 
-        if "dmg" in hse_recov.columns:
-            columns_to_save = ["guid", "d_vacationct", "mhhinck", "pminoritybg", "dmg"] + bval_yr + index_yr
-        else:
-            columns_to_save = ["guid", "d_vacationct", "mhhinck", "pminoritybg", "value_loss"] + bval_yr + index_yr
-
+        columns_to_save = ["guid", "d_vacationct", "mhhinck", "pminoritybg", "dmg", "value_loss"] + bval_yr + index_yr
         self.set_result_csv_data("result", hse_recov[columns_to_save], result_name, "dataframe")
+
 
         return True
 
