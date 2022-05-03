dc994ec1ca0f5b7a68a93788b1b1363173438eef
Chen Wang <cwang138@illinois.edu>
2022-05-03 12:29:10 -0500

    initially rewrite networkdataset class

--- pyincore/networkdataset.py
+++ pyincore/networkdataset.py
@@ -3,10 +3,12 @@
 # This program and the accompanying materials are made available under the
 # terms of the Mozilla Public License v2.0 which accompanies this distribution,
 # and is available at https://www.mozilla.org/en-US/MPL/2.0/
-
+import json
 import os
+
+from pyincore.dataservice import DataService
+
 from pyincore import Dataset
-from pyincore import NetworkData
 
 
 class NetworkDataset:
@@ -19,21 +21,112 @@ class NetworkDataset:
 
     def __init__(self, dataset: Dataset):
         if dataset.format == 'shp-network' and dataset.metadata['networkDataset'] is not None:
-            try:
-                file_path = os.path.join(dataset.local_file_path,
-                                         dataset.metadata['networkDataset']["link"]["fileName"])
-            except FileNotFoundError:
-                raise FileNotFoundError("Invalid local file path.")
-            self.link = NetworkData(network_type=dataset.metadata['networkDataset']["link"]["networkType"],
-                                    file_path=file_path)
-            self.node = NetworkData(network_type=dataset.metadata['networkDataset']["node"]["networkType"],
-                                    file_path=os.path.join(dataset.local_file_path,
-                                                           dataset.metadata['networkDataset']["node"]["fileName"]))
-            self.graph = NetworkData(network_type=dataset.metadata['networkDataset']["graph"]["networkType"],
-                                     file_path=os.path.join(dataset.local_file_path,
-                                                            dataset.metadata['networkDataset']["graph"]["fileName"]))
+            self.node = NetworkDataset._network_component_from_dataset(dataset, "node")
+            self.link = NetworkDataset._network_component_from_dataset(dataset, "link")
+            self.graph = NetworkDataset._network_component_from_dataset(dataset, "graph")
         else:
             self._network_data = None
             self._link = None
             self._node = None
             self._graph = None
+
+    @classmethod
+    def from_data_service(cls, id: str, data_service: DataService):
+        """Get Dataset from Data service, get metadata as well.
+
+        Args:
+            id (str): ID of the Dataset.
+            data_service (obj): Data service.
+
+        Returns:
+            obj: network dataset
+
+        """
+        metadata = data_service.get_dataset_metadata(id)
+        dataset = Dataset(metadata)
+        dataset.cache_files(data_service)
+
+        return cls(dataset)
+
+    @classmethod
+    def from_json_str(cls, json_str, data_service: DataService = None, file_path=None):
+        """Get Dataset from json string.
+
+        Args:
+            json_str (str): JSON of the Dataset.
+            data_service (obj): Data Service class.
+            file_path (str): File path.
+
+        Returns:
+            obj: network dataset
+
+        """
+        dataset = Dataset(json.loads(json_str))
+
+        # download file and set local file path using metadata from services
+        if data_service is not None:
+            dataset.cache_files(data_service)
+        # if there is local files associates with the dataset
+        elif file_path is not None:
+            dataset.local_file_path = file_path
+        else:
+            raise ValueError("You have to either use data services, or given pass local file path.")
+
+        return cls(dataset)
+
+    @classmethod
+    def from_file(cls, file_path, data_type):
+        """Create Dataset from the file.
+
+        Args:
+            file_path (str): File path.
+            data_type (str): Data type.
+
+        Returns:
+            obj: Dataset from file.
+
+        """
+        metadata = {"dataType": data_type,
+                    "format": '',
+                    "fileDescriptors": [],
+                    "id": file_path}
+        dataset = Dataset(metadata)
+        dataset.local_file_path = file_path
+        return cls(dataset)
+
+    @staticmethod
+    def _network_component_from_dataset(dataset: Dataset, network_type="link"):
+        """
+        Create a dataset object for network from another dataset
+        Args:
+            dataset: network dataset
+            network_type: link, node or graph
+
+        Returns: network component in dataset object
+
+        """
+        network_component_metadata = {
+            "dataType": dataset.metadata['networkDataset'][network_type]["networkType"],
+            "format": f"shp-{network_type}"
+        }
+        network_component = Dataset(network_component_metadata)
+        try:
+            link_file_path = os.path.join(dataset.local_file_path,
+                                          dataset.metadata['networkDataset'][network_type]["fileName"])
+        except FileNotFoundError:
+            raise FileNotFoundError("Invalid local file path.")
+        network_component.local_file_path = link_file_path
+
+        return network_component
+
+    @staticmethod
+    def get_inventory_reader(self):
+        """ getter """
+        filename = self.file_path
+        if os.path.isdir(filename):
+            layers = fiona.listlayers(filename)
+            if len(layers) > 0:
+                # for now, open the first shapefile
+                return fiona.open(filename, layer=layers[0])
+        else:
+            return fiona.open(filename)
