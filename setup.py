77073dd8979cbe3024c05f40530031f755c6f23f
YONG WOOK KIM <ywkim312@users.noreply.github.com>
2022-05-10 14:15:43 -0500

    change setup.py for supporting legacy naming convention for package (#139)
    * change setup.py for suppoorting legacy naming convention for package
    
    * fixed the version in setup.py

--- setup.py
+++ setup.py
@@ -5,6 +5,8 @@
 # and is available at https://www.mozilla.org/en-US/MPL/2.0/
 
 from setuptools import setup, find_packages
+import pkg_resources
+pkg_resources.extern.packaging.version.Version = pkg_resources.SetuptoolsLegacyVersion
 
 setup(
     name='pyincore',
