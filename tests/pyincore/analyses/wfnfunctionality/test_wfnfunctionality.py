1249c14f09104c4b30b58c235503577a205f28d8
Santiago Nunez-Corrales <nunezco2@illinois.edu>
2022-07-18 16:39:26 -0500

    WIP: water facility network analysis, test updated

--- tests/pyincore/analyses/wfnfunctionality/test_wfnfunctionality.py
+++ tests/pyincore/analyses/wfnfunctionality/test_wfnfunctionality.py
@@ -2,12 +2,13 @@
 # terms of the Mozilla Public License v2.0 which accompanies this distribution,
 # and is available at https://www.mozilla.org/en-US/MPL/2.0/
 
-from pyincore import IncoreClient, FragilityService, MappingSet, Dataset, DataService
-from pyincore.analyses.waterfacilitydamage import WaterFacilityDamage
+from pyincore import IncoreClient, FragilityService, MappingSet, NetworkDataset, DataService
+from pyincore.analyses.montecarlofailureprobability import MonteCarloFailureProbability
 from pyincore.analyses.wfnfunctionality import WfnFunctionality
 from pyincore.analyses.pipelinedamagerepairrate import PipelineDamageRepairRate
-import pyincore.globals as pyglobals
+from pyincore.analyses.waterfacilitydamage import WaterFacilityDamage
 
+import geopandas as gpd
 
 def run_with_base_class():
     client = IncoreClient()
@@ -29,13 +30,31 @@ def run_with_base_class():
     # First, call water facility damage for existing data
     wf_dmg = WaterFacilityDamage(client)
 
-    # Load water facility inventory dataset
-    wf_dmg.load_remote_input_dataset("water_facilities", wfn_dataset_id)
+    result_name = "wf_dmg_results"
 
-    # Load fragility mapping
+    wf_dmg.set_parameter("result_name", result_name)
+    wf_dmg.set_parameter("hazard_type", hazard_type)
+    wf_dmg.set_parameter("hazard_id", hazard_id)
+    wf_dmg.set_parameter("fragility_key", fragility_key)
+    wf_dmg.set_parameter("use_liquefaction", liquefaction)
+    wf_dmg.set_parameter("liquefaction_geology_dataset_id", liq_geology_dataset_id)
+    wf_dmg.set_parameter("liquefaction_fragility_key", liq_fragility_key)
+    wf_dmg.set_parameter("use_hazard_uncertainty", uncertainty)
+    wf_dmg.set_parameter("num_cpu", 4)
+
     fragility_service = FragilityService(client)
     mapping_set = MappingSet(fragility_service.get_mapping(wf_mapping_id))
+
+    wfn_dataset = NetworkDataset.from_data_service(wfn_dataset_id, data_service)
+    wf_dataset = wfn_dataset.nodes.get_inventory_reader()
+    wf_filepath = wf_dataset.path
+    wf_df = gpd.read_file(wf_filepath)
     wf_dmg.set_input_dataset("dfr3_mapping_set", mapping_set)
+    wf_dmg.set_input_dataset("water_facilities", wf_df)
+
+    wf_dmg.run_analysis()
+
+    wf_dmg_result = wf_dmg.get_output_dataset("result")
 
     # Run pipeline damage
     pipeline_dataset_id = "5a284f28c7d30d13bc081d14"
@@ -43,7 +62,7 @@ def run_with_base_class():
     liq_geology_dataset_id = "5a284f53c7d30d13bc08249c"
     liq_fragility_key = "pgd"
     use_liq = True
-    result_name = "pipeline_result"
+    result_name = "pp_dmg_result"
 
     pipeline_dmg_w_rr = PipelineDamageRepairRate(client)
     pipeline_dmg_w_rr.load_remote_input_dataset("pipeline", pipeline_dataset_id)
@@ -61,15 +80,29 @@ def run_with_base_class():
 
     pipeline_dmg_w_rr.run_analysis()
 
-    pipeline_dmg_result = pipeline_dmg_w_rr.get_output_dataset("result")
-    pipeline_dmg_df = pipeline_dmg_result.get_dataframe_from_csv()
+    pipeline_dmg_fs = pipeline_dmg_w_rr.get_output_dataset("sample_failure_state")
+
+    # Using water facility damage, run a Monte Carlo analysis
+    mc = MonteCarloFailureProbability(client)
+
+    mc.set_input_dataset("damage", wf_dmg_result)
+    mc.set_parameter("result_name", "wf_dmg_mc")
+    mc.set_parameter("num_cpu", 8)
+    nsamp = 20000
+    mc.set_parameter("num_samples", nsamp)
+    mc.set_parameter("damage_interval_keys", ["DS_0", "DS_1", "DS_2", "DS_3", "DS_4"])
+    mc.set_parameter("failure_state_keys", ["DS_3", "DS_4"])
+
+    mc.run_analysis()
+
+    wf_dmg_fs = mc.get_output_dataset("sample_failure_state")
 
     # Run wfn functionality
     wfn_func = WfnFunctionality(client)
 
     wfn_func.load_remote_input_dataset("wfn_network", wfn_dataset_id)
-    #wfn_func.load_remote_input_dataset("wf_sample_failure_state", "62d03711861e370172cb0a37")
-    wfn_func.set_input_dataset("pp_sample_failure_state", pipeline_dmg_df)
+    wfn_func.set_input_dataset("wf_sample_failure_state", wf_dmg_fs)
+    wfn_func.set_input_dataset("pp_sample_failure_state", pipeline_dmg_fs)
     wfn_func.set_parameter("result_name", "mmsa_wfn_functionality")
     wfn_func.set_parameter("tank_node_list", [1, 7, 10, 13, 14, 15])
     wfn_func.set_parameter("pumpstation_node_list", [2, 3, 4, 5, 6, 8, 9, 11, 12])
