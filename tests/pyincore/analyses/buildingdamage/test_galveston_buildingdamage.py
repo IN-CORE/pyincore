a9f8adc6dfa36a8eb7dcd761525b5a4d8594a640
Chen Wang <cwang138@illinois.edu>
2022-08-23 15:55:05 -0500

    update the ids

--- tests/pyincore/analyses/buildingdamage/test_galveston_buildingdamage.py
+++ tests/pyincore/analyses/buildingdamage/test_galveston_buildingdamage.py
@@ -6,11 +6,10 @@ from pyincore import Dataset
 
 def run_with_base_class():
     client = IncoreClient(pyglobals.INCORE_API_DEV_URL)
-    buildings = Dataset.from_file("/Users/cwang138/Documents/INCORE-2.0/Galveston/galveston_bldgs",
-                                  "ergo:buildingInventoryVer7")
+    client.clear_cache()
 
     bldg_dmg = BuildingDamage(client)
-    bldg_dmg.set_input_dataset("buildings", buildings)
+    bldg_dmg.load_remote_input_dataset("buildings", "63053e74c8f8b7614f6cff9f")
 
     hazard_type = "hurricane"
     hazard_id = "5fa472033c1f0c73fe81461a"
@@ -33,7 +32,7 @@ def run_with_base_class():
     # hurricane flood
     flood_mapping_id = "62fefd688a30d30dac57bbd7"
     fragility_service = FragilityService(client)
-    mapping_set = MappingSet(fragility_service.get_mapping(wind_mapping_id))
+    mapping_set = MappingSet(fragility_service.get_mapping(flood_mapping_id))
     bldg_dmg.set_input_dataset('dfr3_mapping_set', mapping_set)
 
     bldg_dmg.set_parameter("result_name", "galveston_hurricane_flood_bldg_dmg")
@@ -48,7 +47,7 @@ def run_with_base_class():
     # hurricane surge-wave
     sw_mapping_id = "6303e51bd76c6d0e1f6be080"
     fragility_service = FragilityService(client)
-    mapping_set = MappingSet(fragility_service.get_mapping(wind_mapping_id))
+    mapping_set = MappingSet(fragility_service.get_mapping(sw_mapping_id))
     bldg_dmg.set_input_dataset('dfr3_mapping_set', mapping_set)
 
     bldg_dmg.set_parameter("result_name", "galveston_hurricane_sw_bldg_dmg")
