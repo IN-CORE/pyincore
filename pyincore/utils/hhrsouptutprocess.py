8b0737386bdbb04e1d3c318fe9035bf35e15c843
Chen Wang <cwang138@illinois.edu>
2023-01-27 09:32:25 -0600

    store the json file

--- pyincore/utils/hhrsouptutprocess.py
+++ pyincore/utils/hhrsouptutprocess.py
@@ -3,29 +3,34 @@
 # This program and the accompanying materials are made available under the
 # terms of the Mozilla Public License v2.0 which accompanies this distribution,
 # and is available at https://www.mozilla.org/en-US/MPL/2.0/
+import json
 
 
 class HHRSOutputPRocess:
     """This class converts results outputs of housing household recovery sequential analysis to certain format."""
 
     @staticmethod
-    def get_hhrs_stage_count(timesteps, hhrs_df):
+    def get_hhrs_stage_count(timesteps, hhrs_df, filename_json="hhrs_stage_count.json"):
         """
 
         Args:
             timesteps: timesteps in the unit of month ["0", "6", "12", "24", "48", etc]
             hhrs_df: pandas dataframe of the output of housingrecoverysequential
+            filename_json: the name of the json file to store the output
 
         Returns:
 
         """
-
-        # hhr_result = housing_recovery.get_output_dataset("ds_result")
-        # hhrs_df = hhr_result.get_dataframe_from_csv()
 
         hhrs_stage_count = {}
         for t in timesteps:
             stage = hhrs_df[t]
             hhrs_stage_count[t] = [(stage == 1.0).sum(), (stage == 2.0).sum(), (stage == 3.0).sum(),
                                    (stage == 4.0).sum(), (stage == 5.0).sum()]
+
+        if filename_json:
+            with open(filename_json, "w") as outfile:
+                json.dump(hhrs_stage_count, outfile)
+
         return hhrs_stage_count
+
