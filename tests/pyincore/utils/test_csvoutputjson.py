4795bc3bd17b4b57954f27679fca9892e90c094c
Chen Wang <cwang138@illinois.edu>
2023-02-02 12:06:28 -0600

    fix test_csvoutputjson.py

--- tests/pyincore/utils/test_csvoutputjson.py
+++ tests/pyincore/utils/test_csvoutputjson.py
@@ -13,19 +13,25 @@ def run_convert_cge_json():
     cge_json = CGEOutputProcess()
     filepath = os.path.join(PYINCOREPATH, TESTAPATH, "joplincge")
 
-    cge_json.get_cge_household_count(None, ["HH1", "HH2", "HH3", "HH4", "HH5"],
-                                     os.path.join(filepath, "joplin-pop-disl-results.csv"),
-                                     "cge_total_household_count.json")
-    cge_json.get_cge_gross_income(None, ["HH1", "HH2", "HH3", "HH4", "HH5"],
+    cge_json.get_cge_household_count(None,
+                                     os.path.join(filepath, "household-count.csv"),
+                                     "cge_total_household_count.json",
+                                     ["HH1", "HH2", "HH3", "HH4", "HH5"]
+                                     )
+    cge_json.get_cge_gross_income(None,
                                   os.path.join(filepath, "gross-income.csv"),
-                                  "cge_total_household_income.json")
-    cge_json.get_cge_employment(None, None, ["Goods", "Trade", "Other"],
+                                  "cge_total_household_income.json",
+                                  ["HH1", "HH2", "HH3", "HH4", "HH5"])
+    cge_json.get_cge_employment(None, None,
                                 os.path.join(filepath, "pre-disaster-factor-demand.csv"),
                                 os.path.join(filepath, "post-disaster-factor-demand.csv"),
-                                "cge_employment.json")
-    cge_json.get_cge_domestic_supply(None, ["Goods", "Trade", "Other", "HS1", "HS2", "HS3"],
+                                "cge_employment.json",
+                                ["GOODS", "TRADE", "OTHER"]
+                                )
+    cge_json.get_cge_domestic_supply(None,
                                      os.path.join(filepath, "domestic-supply.csv"),
-                                     "cge_domestic_supply.json")
+                                     "cge_domestic_supply.json",
+                                     ["GOODS", "TRADE", "OTHER", "HS1", "HS2", "HS3"])
     return True
 
 
