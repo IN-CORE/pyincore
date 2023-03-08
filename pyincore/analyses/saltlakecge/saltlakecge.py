51715b3395fe5e269aa224d146d2ac559e245414
Chris Navarro <cmnavarr@illinois.edu>
2023-03-08 09:57:09 -0600

    Use solver_path for IPOPT if set by the user

--- pyincore/analyses/saltlakecge/saltlakecge.py
+++ pyincore/analyses/saltlakecge/saltlakecge.py
@@ -1745,7 +1745,13 @@ class SaltLakeCGEModel(BaseAnalysis):
           solver_io = 'nl'
           stream_solver = True     # True prints solver output to screen
           keepfiles = False    # True prints intermediate file names (.nl,.sol,...)
-          opt = SolverFactory(solver, solver_io= solver_io)
+
+          executable_path = self.get_parameter("solver_path") \
+              if self.get_parameter("solver_path") is not None else pyglobals.IPOPT_PATH
+          if not os.path.exists(executable_path):
+              print("Invalid executable path, please make sure you have Pyomo installed.")
+
+          opt = SolverFactory(solver, solver_io= solver_io, executable=executable_path)
 
           if opt is None:
               print("")
