d342410a0b08a3d993309640d0096e962a311d23
Vismayak Mohanarajan <vismayak97@gmail.com>
2023-03-17 10:38:02 -0500

    Update dataservice.py

--- pyincore/dataservice.py
+++ pyincore/dataservice.py
@@ -215,14 +215,15 @@ class DataService:
         r = self.client.put(url, **kwargs)
         return r.json()
 
-    def add_files_to_dataset(self, dataset_id: str, filepaths: list, timeout=(30,600)):
+    def add_files_to_dataset(self, dataset_id: str, filepaths: list, timeout=(30,600), **kwargs):
         """Add files to the dataset. Post API endpoint is called.
 
         Args:
             dataset_id (str): ID of the Dataset.
             filepaths (list): Path to the files.
             timeout (tuple[int,int]): Session timeout.
-            
+            **kwargs: A dictionary of external parameters.
+
         Returns:
             obj: HTTP POST Response. Json of the files added to the dataset.
 
@@ -233,7 +234,7 @@ class DataService:
             file = open(filepath, 'rb')
             tuple = ('file', file)
             listfiles.append(tuple)
-        kwargs = {"files": listfiles}
+        kwargs['files'] = listfiles
         r = self.client.post(url, timeout=timeout, **kwargs)
 
         # close files
