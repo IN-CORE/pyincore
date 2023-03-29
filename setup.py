073ea4f94c6b98fe61c1596838b4709b67bb0683
Yong Wook Kim <ywkim@illinois.edu>
2023-03-29 16:12:36 -0500

    change version number

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@
 from setuptools import setup, find_packages
 
 # version number of pyincore
-version = '1.9.0'
+version = '1.10.0'
 
 with open("README.rst", encoding="utf-8") as f:
     readme = f.read()
