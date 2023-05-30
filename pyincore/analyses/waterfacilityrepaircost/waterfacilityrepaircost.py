0c08fc0916a8013255580fbfc70e5bca267ebcf4
Chen Wang <cwang138@illinois.edu>
2023-05-30 10:44:17 -0500

    initialize water facility repair cost

--- pyincore/analyses/waterfacilityrepaircost/waterfacilityrepaircost.py
+++ pyincore/analyses/waterfacilityrepaircost/waterfacilityrepaircost.py
@@ -10,8 +10,8 @@ from pyincore import AnalysisUtil
 from pyincore import BaseAnalysis
 
 
-class EpfRepairCost(BaseAnalysis):
-    """Computes electric power facility repair cost.
+class WaterFacilityRepairCost(BaseAnalysis):
+    """Computes water facility repair cost.
 
     Args:
         incore_client (IncoreClient): Service authentication.
@@ -19,41 +19,41 @@ class EpfRepairCost(BaseAnalysis):
     """
 
     def __init__(self, incore_client):
-        super(EpfRepairCost, self).__init__(incore_client)
+        super(WaterFacilityRepairCost, self).__init__(incore_client)
 
     def run(self):
-        """Executes electric power facility repair cost analysis."""
+        """Executes water facility repair cost analysis."""
 
-        epf_df = self.get_input_dataset("epfs").get_dataframe_from_shapefile()
+        wf_df = self.get_input_dataset("water_facilities").get_dataframe_from_shapefile()
         sample_damage_states_df = self.get_input_dataset("sample_damage_states").get_dataframe_from_csv()
         replacement_cost = self.get_input_dataset("replacement_cost").get_dataframe_from_csv()
 
         # join damage state, replacement cost, with original inventory
-        epf_df = epf_df.merge(sample_damage_states_df, on="guid")
-        epf_df = epf_df.merge(replacement_cost, on="guid")
-        epf_set = epf_df.to_dict(orient="records")
+        wf_df = wf_df.merge(sample_damage_states_df, on="guid")
+        wf_df = wf_df.merge(replacement_cost, on="guid")
+        wf_set = wf_df.to_dict(orient="records")
 
         user_defined_cpu = 1
         if not self.get_parameter("num_cpu") is None and self.get_parameter("num_cpu") > 0:
             user_defined_cpu = self.get_parameter("num_cpu")
 
-        num_workers = AnalysisUtil.determine_parallelism_locally(self, len(epf_set), user_defined_cpu)
+        num_workers = AnalysisUtil.determine_parallelism_locally(self, len(wf_set), user_defined_cpu)
 
-        avg_bulk_input_size = int(len(epf_set) / num_workers)
+        avg_bulk_input_size = int(len(wf_set) / num_workers)
         inventory_args = []
         count = 0
-        inventory_list = list(epf_set)
+        inventory_list = list(wf_set)
         while count < len(inventory_list):
             inventory_args.append(inventory_list[count:count + avg_bulk_input_size])
             count += avg_bulk_input_size
 
-        repair_costs = self.epf_repair_cost_concurrent_future(self.epf_repair_cost_bulk_input, num_workers,
-                                                              inventory_args)
+        repair_costs = self.wf_repair_cost_concurrent_future(self.wf_repair_cost_bulk_input, num_workers,
+                                                             inventory_args)
         self.set_result_csv_data("result", repair_costs, name=self.get_parameter("result_name") + "_repair_cost")
 
         return True
 
-    def epf_repair_cost_concurrent_future(self, function_name, num_workers, *args):
+    def wf_repair_cost_concurrent_future(self, function_name, num_workers, *args):
         """Utilizes concurrent.future module.
 
         Args:
@@ -62,7 +62,7 @@ class EpfRepairCost(BaseAnalysis):
             *args: All the arguments in order to pass into parameter function_name.
 
         Returns:
-            list: A list of ordered dictionaries with epf repair cost values and other data/metadata.
+            list: A list of ordered dictionaries with water facility repair cost values and other data/metadata.
 
         """
 
@@ -73,53 +73,53 @@ class EpfRepairCost(BaseAnalysis):
 
         return output
 
-    def epf_repair_cost_bulk_input(self, epfs):
-        """Run analysis for multiple epfs.
+    def wf_repair_cost_bulk_input(self, water_facilities):
+        """Run analysis for multiple water facilities.
 
         Args:
-            epfs (list): Multiple epfs from input inventory set.
+            water_facilities (list): Multiple water facilities from input inventory set.
 
         Returns:
-            list: A list of ordered dictionaries with epf repair cost values and other data/metadata.
+            list: A list of ordered dictionaries with water facility repair cost values and other data/metadata.
 
         """
         # read in the damage ratio tables
-        epf_dmg_ratios_csv = self.get_input_dataset("epf_dmg_ratios").get_csv_reader()
-        dmg_ratio_tbl = AnalysisUtil.get_csv_table_rows(epf_dmg_ratios_csv, ignore_first_row=False)
+        wf_dmg_ratios_csv = self.get_input_dataset("wf_dmg_ratios").get_csv_reader()
+        dmg_ratio_tbl = AnalysisUtil.get_csv_table_rows(wf_dmg_ratios_csv, ignore_first_row=False)
 
         repair_costs = []
 
-        for epf in epfs:
+        for wf in water_facilities:
             rc = dict()
-            rc["guid"] = epf["guid"]
-            epf_type = epf["utilfcltyc"]
+            rc["guid"] = wf["guid"]
+            wf_type = wf["utilfcltyc"]
 
-            sample_damage_states = epf["sample_damage_states"].split(",")
+            sample_damage_states = wf["sample_damage_states"].split(",")
             repair_cost = ["0"] * len(sample_damage_states)
             for n, ds in enumerate(sample_damage_states):
                 for dmg_ratio_row in dmg_ratio_tbl:
                     # use "in" instead of "==" since some inventory has pending number (e.g. EDC2)
-                    if dmg_ratio_row["Inventory Type"] in epf_type and dmg_ratio_row["Damage State"] == ds:
+                    if dmg_ratio_row["Inventory Type"] in wf_type and dmg_ratio_row["Damage State"] == ds:
                         dr = float(dmg_ratio_row["Best Mean Damage Ratio"])
-                        repair_cost[n] = str(epf["replacement_cost"] * dr)
+                        repair_cost[n] = str(wf["replacement_cost"] * dr)
 
-            rc["budget"] = ','.join(repair_cost)
-            rc["repaircost"] = ','.join(repair_cost)
+            rc["budget"] = ",".join(repair_cost)
+            rc["repaircost"] = ",".join(repair_cost)
 
             repair_costs.append(rc)
 
         return repair_costs
 
     def get_spec(self):
-        """Get specifications of the epf repair cost analysis.
+        """Get specifications of the water facility repair cost analysis.
 
         Returns:
-            obj: A JSON object of specifications of the epf repair cost analysis.
+            obj: A JSON object of specifications of the water facility repair cost analysis.
 
         """
         return {
-            "name": "epf-repair-cost",
-            "description": "Electric Power Facility repair cost analysis.",
+            "name": "wf-repair-cost",
+            "description": "Water Facility repair cost analysis.",
             "input_parameters": [
                 {
                     "id": "result_name",
@@ -136,13 +136,10 @@ class EpfRepairCost(BaseAnalysis):
             ],
             "input_datasets": [
                 {
-                    "id": "epfs",
+                    "id": "water_facilities",
                     "required": True,
-                    "description": "Electric Power Facility Inventory",
-                    "type": ["incore:epf",
-                             "ergo:epf",
-                             "incore:epfVer2"
-                             ],
+                    "description": "Water Facility Inventory",
+                    "type": ["ergo:waterFacilityTopo"],
                 },
                 {
                     "id": "replacement_cost",
@@ -157,17 +154,17 @@ class EpfRepairCost(BaseAnalysis):
                     "type": ["incore:sampleDamageState"]
                 },
                 {
-                    "id": "epf_dmg_ratios",
+                    "id": "wf_dmg_ratios",
                     "required": True,
                     "description": "Damage Ratios table",
-                    "type": ["incore:epfDamageRatios"]
+                    "type": ["incore:waterFacilityDamageRatios"]
                 },
             ],
             "output_datasets": [
                 {
                     "id": "result",
-                    "parent_type": "epfs",
-                    "description": "A csv file with repair cost for each electric power facility",
+                    "parent_type": "water_facilities",
+                    "description": "A csv file with repair cost for each water facility",
                     "type": "incore:repairCost"
                 }
             ]
---
+++ tests/pyincore/analyses/waterfacilityrepaircost/test_waterfacilityrepaircost.py
@@ -0,0 +1,32 @@
+from pyincore import IncoreClient
+from pyincore.analyses.epfrepaircost import EpfRepairCost
+import pyincore.globals as pyglobals
+
+
+def run_with_base_class():
+    client = IncoreClient(pyglobals.INCORE_API_DEV_URL)
+    # client = IncoreClient()
+
+    epf_repair_cost = EpfRepairCost(client)
+
+    # Seaside EPF
+    epf_repair_cost.load_remote_input_dataset("epfs", "5eebcaa17a00803abc85ec11")  # dev
+    # epf_repair_cost.load_remote_input_dataset("epfs", "5d263f08b9219cf93c056c68")  # prod
+
+    epf_repair_cost.load_remote_input_dataset("replacement_cost", "6470c09a5bc8b26ddf99bb59")
+
+    # can be chained with MCS
+    epf_repair_cost.load_remote_input_dataset("sample_damage_states", "6470c23d5bc8b26ddf99bb65")
+
+    # substation_dmg_ratios
+    epf_repair_cost.load_remote_input_dataset("epf_dmg_ratios", "6470c1c35bc8b26ddf99bb5f")
+
+    epf_repair_cost.set_parameter("result_name", "seaside_epf")
+    epf_repair_cost.set_parameter("num_cpu", 4)
+
+    # Run Analysis
+    epf_repair_cost.run_analysis()
+
+
+if __name__ == "__main__":
+    run_with_base_class()
