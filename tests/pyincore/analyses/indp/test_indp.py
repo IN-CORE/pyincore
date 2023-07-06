c58f6f6d80f4d030718cc35fbe93ec23e8485183
Chen Wang <cwang138@illinois.edu>
2023-07-06 14:09:27 -0500

    repair time is tricky to wire in properly...

--- tests/pyincore/analyses/indp/test_indp.py
+++ tests/pyincore/analyses/indp/test_indp.py
@@ -1,6 +1,7 @@
 import pandas as pd
 
 from pyincore import IncoreClient, Dataset, RestorationService, MappingSet, FragilityService
+from pyincore.analyses.epfrestoration import EpfRestoration, EpfRestorationUtil
 from pyincore.analyses.indp import INDP
 import pyincore.globals as pyglobals
 from pyincore.analyses.waterfacilitydamage import WaterFacilityDamage
@@ -9,105 +10,141 @@ from pyincore.analyses.pipelinedamagerepairrate import pipelinedamagerepairrate,
 from pyincore.analyses.montecarlofailureprobability import MonteCarloFailureProbability
 from pyincore.analyses.pipelinefunctionality import PipelineFunctionality
 from pyincore.analyses.waterfacilityrestoration import WaterFacilityRestoration, WaterFacilityRestorationUtil
-
+from pyincore.dataservice import DataService
 
 def run_with_base_class():
     dev_client = IncoreClient(pyglobals.INCORE_API_DEV_URL)
     prod_client = IncoreClient()
+    dev_datasvc = DataService(dev_client)
+    prod_datasvc = DataService(prod_client)
+
     hazard_type = "earthquake"
     hazard_id = "5dfa3e36b9219c934b64c231"  # 1000 yr eq
+    epfs_id = "5d263f08b9219cf93c056c68"
     num_cpu = 8
     sim_number = 10
     # sample_range = range(0, sim_number)
     sample_range = range(0, 1)
     result_name = "seaside_indp"
 
+    ###################################################
+    # water facility damage
+    ###################################################
+    wterfclty_dmg = WaterFacilityDamage(prod_client)
+    fragility_service = FragilityService(prod_client)
+    wterfclty_dmg.load_remote_input_dataset("water_facilities", "60e5e91960b3f41243faa3b2")
+    mapping_id = "5d39e010b9219cc18bd0b0b6"  # 5 DS
+    mapping_set = MappingSet(fragility_service.get_mapping(mapping_id))
+    wterfclty_dmg.set_input_dataset("dfr3_mapping_set", mapping_set)
+    wterfclty_dmg.set_parameter("hazard_type", hazard_type)
+    wterfclty_dmg.set_parameter("fragility_key", "pga")
+    wterfclty_dmg.set_parameter("num_cpu", 4)
+    wterfclty_dmg.set_parameter("hazard_id", hazard_id)
+    wterfclty_dmg.set_parameter("result_name", result_name)
+    wterfclty_dmg.run_analysis()
+    wterfclty_dmg_result = wterfclty_dmg.get_output_dataset("result")
+
+    ###################################################
+    # water facility mcs
+    ###################################################
+    wterfclty_mc = MonteCarloFailureProbability(prod_client)
+    wterfclty_mc.set_input_dataset("damage", wterfclty_dmg_result)
+    wterfclty_mc.set_parameter("num_cpu", num_cpu)
+    wterfclty_mc.set_parameter("num_samples", sim_number)
+    wterfclty_mc.set_parameter("damage_interval_keys", ["DS_0", "DS_1", "DS_2", "DS_3", "DS_4"])
+    wterfclty_mc.set_parameter("failure_state_keys", ["DS_1", "DS_2", "DS_3", "DS_4"])
+    wterfclty_mc.set_parameter("result_name", result_name + "_wf")  # name of csv file with results
+    wterfclty_mc.run()
+    wterfclty_sample_failure_state = wterfclty_mc.get_output_dataset("sample_failure_state")
+
+    ###################################################
+    # epf damage
+    ###################################################
+    epf_dmg = EpfDamage(prod_client)
+    fragility_service = FragilityService(prod_client)
+    epf_dmg.load_remote_input_dataset("epfs", epfs_id)
+    mapping_id = "5d489aa1b9219c0689f1988e"  # 5 DS
+    mapping_set = MappingSet(fragility_service.get_mapping(mapping_id))
+    epf_dmg.set_input_dataset('dfr3_mapping_set', mapping_set)
+    epf_dmg.set_parameter("hazard_type", hazard_type)
+    epf_dmg.set_parameter("num_cpu", num_cpu)
+    epf_dmg.set_parameter('fragility_key', "pga")
+    epf_dmg.set_parameter("hazard_id", hazard_id)
+    epf_dmg.set_parameter("result_name", result_name)
+    epf_dmg.run_analysis()
+    epf_dmg_result = epf_dmg.get_output_dataset("result")
+
     # ###################################################
-    # # water facility damage
+    # # epf repair time
     # ###################################################
-    # wterfclty_dmg = WaterFacilityDamage(prod_client)
-    # fragility_service = FragilityService(prod_client)
-    # wterfclty_dmg.load_remote_input_dataset("water_facilities", "60e5e91960b3f41243faa3b2")
-    # mapping_id = "5d39e010b9219cc18bd0b0b6"  # 5 DS
-    # mapping_set = MappingSet(fragility_service.get_mapping(mapping_id))
-    # wterfclty_dmg.set_input_dataset("dfr3_mapping_set", mapping_set)
-    # wterfclty_dmg.set_parameter("hazard_type", hazard_type)
-    # wterfclty_dmg.set_parameter("fragility_key", "pga")
-    # wterfclty_dmg.set_parameter("num_cpu", 4)
-    # wterfclty_dmg.set_parameter("hazard_id", hazard_id)
-    # wterfclty_dmg.set_parameter("result_name", result_name)
-    # wterfclty_dmg.run_analysis()
-    # wterfclty_dmg_result = wterfclty_dmg.get_output_dataset("result")
+    # epf_rest = EpfRestoration(prod_client)
+    # restorationsvc = RestorationService(prod_client)
+    # mapping_set = MappingSet(restorationsvc.get_mapping("61f302e6e3a03e465500b3eb"))
+    # epf_rest.load_remote_input_dataset("epfs", epfs_id)
+    # epf_rest.set_input_dataset("dfr3_mapping_set", mapping_set)
+    # epf_rest.set_input_dataset('damage', epf_dmg_result)
+    # epf_rest.set_parameter("result_name", "seaside_indp")
+    # epf_rest.set_parameter("discretized_days", [1, 3, 7, 30, 90])
+    # epf_rest.set_parameter("restoration_key", "Restoration ID Code")
+    # epf_rest.set_parameter("end_time", 100.0)
+    # epf_rest.set_parameter("time_interval", 1.0)
+    # epf_rest.set_parameter("pf_interval", 0.01)
+    # epf_rest.run_analysis()
+    # inventory_restoration_map = epf_rest.get_output_dataset("inventory_restoration_map")
+    # pf_results = epf_rest.get_output_dataset("pf_results")
+    # time_results = epf_rest.get_output_dataset("time_results")
+    # time_interval = epf_rest.get_parameter("time_interval")
+    # pf_interval = epf_rest.get_parameter("pf_interval")
+    # end_time = epf_rest.get_parameter("end_time")
+    # epf_rest_util = EpfRestorationUtil(inventory_restoration_map, pf_results, time_results, time_interval,
+    #                                    pf_interval, end_time)
     #
-    # ###################################################
-    # # water facility mcs
-    # ###################################################
-    # wterfclty_mc = MonteCarloFailureProbability(prod_client)
-    # wterfclty_mc.set_input_dataset("damage", wterfclty_dmg_result)
-    # wterfclty_mc.set_parameter("num_cpu", num_cpu)
-    # wterfclty_mc.set_parameter("num_samples", sim_number)
-    # wterfclty_mc.set_parameter("damage_interval_keys", ["DS_0", "DS_1", "DS_2", "DS_3", "DS_4"])
-    # wterfclty_mc.set_parameter("failure_state_keys", ["DS_1", "DS_2", "DS_3", "DS_4"])
-    # wterfclty_mc.set_parameter("result_name", result_name + "_wf")  # name of csv file with results
-    # wterfclty_mc.run()
-    # wterfclty_sample_failure_state = wterfclty_mc.get_output_dataset("sample_failure_state")
-    #
-    # ###################################################
-    # # epf damage
-    # ###################################################
-    # epf_dmg = EpfDamage(prod_client)
-    # fragility_service = FragilityService(prod_client)
-    # epf_dmg.load_remote_input_dataset("epfs", "5d263f08b9219cf93c056c68")
-    # mapping_id = "5d489aa1b9219c0689f1988e"  # 5 DS
-    # mapping_set = MappingSet(fragility_service.get_mapping(mapping_id))
-    # epf_dmg.set_input_dataset('dfr3_mapping_set', mapping_set)
-    # epf_dmg.set_parameter("hazard_type", hazard_type)
-    # epf_dmg.set_parameter("num_cpu", num_cpu)
-    # epf_dmg.set_parameter('fragility_key', "pga")
-    # epf_dmg.set_parameter("hazard_id", hazard_id)
-    # epf_dmg.set_parameter("result_name", result_name)
-    # epf_dmg.run_analysis()
-    # epf_dmg_result = epf_dmg.get_output_dataset("result")
-    #
-    # ###################################################
-    # # epf mcs
-    # ###################################################
-    # epf_mc = MonteCarloFailureProbability(prod_client)
-    # epf_mc.set_input_dataset("damage", epf_dmg_result)
-    # epf_mc.set_parameter("num_cpu", num_cpu)
-    # epf_mc.set_parameter("num_samples", sim_number)
-    # epf_mc.set_parameter("damage_interval_keys", ["DS_0", "DS_1", "DS_2", "DS_3", "DS_4"])
-    # epf_mc.set_parameter("failure_state_keys", ["DS_1", "DS_2", "DS_3", "DS_4"])
-    # epf_mc.set_parameter("result_name", result_name + "_epf")  # name of csv file with results
-    # epf_mc.run()
-    # epf_sample_failure_state = epf_mc.get_output_dataset("sample_failure_state")
-    #
-    # ###################################################
-    # # pipeline repair rate damage
-    # ###################################################
-    # pipeline_dmg = PipelineDamageRepairRate(prod_client)
-    # fragility_service = FragilityService(prod_client)
-    # pipeline_dmg.load_remote_input_dataset("pipeline", "60e72f9fd3c92a78c89636c7")
-    # mapping_id = "5b47c227337d4a38464efea8"
-    # mapping_set = MappingSet(fragility_service.get_mapping(mapping_id))
-    # pipeline_dmg.set_input_dataset('dfr3_mapping_set', mapping_set)
-    # pipeline_dmg.set_parameter("hazard_type", hazard_type)
-    # pipeline_dmg.set_parameter("fragility_key", 'pgv')
-    # pipeline_dmg.set_parameter("num_cpu", num_cpu)
-    # pipeline_dmg.set_parameter("hazard_id", hazard_id)
-    # pipeline_dmg.set_parameter("result_name", result_name)
-    # pipeline_dmg.run_analysis()
-    # pipeline_dmg_result = pipeline_dmg.get_output_dataset("result")
-    #
-    # ###################################################
-    # # pipeline functionality
-    # ###################################################
-    # pipeline_func = PipelineFunctionality(prod_client)
-    # pipeline_func.set_input_dataset("pipeline_repair_rate_damage", pipeline_dmg_result)
-    # pipeline_func.set_parameter("result_name", result_name + "_pipeline")
-    # pipeline_func.set_parameter("num_samples", sim_number)
-    # pipeline_func.run_analysis()
-    # pipeline_sample_failure_state = pipeline_func.get_output_dataset("sample_failure_state")
+    # epf_df = Dataset.from_data_service("epfs_id", prod_datasvc).get_dataframe_from_shapefile()
+    # epf_repair_time_df = pd.DataFrame()
+    # for index, row in epf_df.iterrows:
+    #     temp_dict = {'repairtime': epf_rest_util.get_restoration_time(guid=row["guid"], damage_state="DS_1", pf=0.99),
+    #                  'guid': row["guid"]}
+    #     epf_repair_time_df = epf_repair_time_df.append(temp_dict, ignore_index=True)
+
+    ###################################################
+    # epf mcs
+    ###################################################
+    epf_mc = MonteCarloFailureProbability(prod_client)
+    epf_mc.set_input_dataset("damage", epf_dmg_result)
+    epf_mc.set_parameter("num_cpu", num_cpu)
+    epf_mc.set_parameter("num_samples", sim_number)
+    epf_mc.set_parameter("damage_interval_keys", ["DS_0", "DS_1", "DS_2", "DS_3", "DS_4"])
+    epf_mc.set_parameter("failure_state_keys", ["DS_1", "DS_2", "DS_3", "DS_4"])
+    epf_mc.set_parameter("result_name", result_name + "_epf")  # name of csv file with results
+    epf_mc.run()
+    epf_sample_failure_state = epf_mc.get_output_dataset("sample_failure_state")
+
+    ###################################################
+    # pipeline repair rate damage
+    ###################################################
+    pipeline_dmg = PipelineDamageRepairRate(prod_client)
+    fragility_service = FragilityService(prod_client)
+    pipeline_dmg.load_remote_input_dataset("pipeline", "60e72f9fd3c92a78c89636c7")
+    mapping_id = "5b47c227337d4a38464efea8"
+    mapping_set = MappingSet(fragility_service.get_mapping(mapping_id))
+    pipeline_dmg.set_input_dataset('dfr3_mapping_set', mapping_set)
+    pipeline_dmg.set_parameter("hazard_type", hazard_type)
+    pipeline_dmg.set_parameter("fragility_key", 'pgv')
+    pipeline_dmg.set_parameter("num_cpu", num_cpu)
+    pipeline_dmg.set_parameter("hazard_id", hazard_id)
+    pipeline_dmg.set_parameter("result_name", result_name)
+    pipeline_dmg.run_analysis()
+    pipeline_dmg_result = pipeline_dmg.get_output_dataset("result")
+
+    ###################################################
+    # pipeline functionality
+    ###################################################
+    pipeline_func = PipelineFunctionality(prod_client)
+    pipeline_func.set_input_dataset("pipeline_repair_rate_damage", pipeline_dmg_result)
+    pipeline_func.set_parameter("result_name", result_name + "_pipeline")
+    pipeline_func.set_parameter("num_samples", sim_number)
+    pipeline_func.run_analysis()
+    pipeline_sample_failure_state = pipeline_func.get_output_dataset("sample_failure_state")
 
     ###################################################
     # INDP
@@ -161,9 +198,6 @@ def run_with_base_class():
     indp_analysis.set_input_dataset("pipeline_failure_state", pipeline_sample_failure_state)
     epf_sample_failure_state = Dataset.from_file("data/seaside_indp_epf_failure_state.csv", "incore:sampleFailureState")
     indp_analysis.set_input_dataset("epf_failure_state", epf_sample_failure_state)
-
-    # indp_analysis.load_remote_input_dataset("initial_node", "61c1015e837ac508f9a178f5")
-    # indp_analysis.load_remote_input_dataset("initial_link", "61c101a6837ac508f9a178fb")
 
     # # optional inputs
     # indp_analysis.load_remote_input_dataset("bldgs2elec", "61c10219837ac508f9a17904")
