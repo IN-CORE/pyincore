61ff484287d3df2b37b49bad69dd1dc7e887dc74
Chen Wang <cwang138@illinois.edu>
2023-10-09 09:06:28 -0500

    changelog

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -35,7 +35,7 @@ author = ''
 # The short X.Y version
 version = '1.12'
 # The full version, including alpha/beta/rc tags
-release = '1.12.0'
+release = '1.13.0'
 
 # -- General configuration ---------------------------------------------------
 
