3c446aeb84de103b81dfb2b529fb8be43015ae83
Chen Wang <cwang138@illinois.edu>
2023-10-26 11:19:30 -0500

    write common case

--- pyincore/analyses/buildingdamage/buildingdamage.py
+++ pyincore/analyses/buildingdamage/buildingdamage.py
@@ -41,12 +41,24 @@ class BuildingDamage(BaseAnalysis):
         else:
             retrofit_strategy = None
 
-        # Accomodating to multihazard
-        # Get hazard input
-        hazard_dataset_ids = self.get_parameter("hazard_id").split("+")
+        # Accommodating to multi-hazard
+        hazards = []  # hazard objects
+        hazard_object = self.get_input_hazard("hazard")
 
-        # Hazard type of the exposure
-        hazard_types = self.get_parameter("hazard_type").split("+")
+        # To use local hazard
+        if hazard_object is not None:
+            # Right now only supports single hazard for local hazard object
+            hazard_types = [hazard_object.hazard_type]
+            hazard_dataset_ids = [hazard_object.id]
+            hazards = [hazard_object]
+        # To use remote hazard
+        elif self.get_parameter("hazard_id") is not None and self.get_parameter("hazard_type") is not None:
+            hazard_dataset_ids = self.get_parameter("hazard_id").split("+")
+            hazard_types = self.get_parameter("hazard_type").split("+")
+            for hazard_type, hazard_dataset_id in zip(hazard_types, hazard_dataset_ids):
+                hazards.append(BaseAnalysis._create_hazard_object(hazard_type, hazard_dataset_id, self.hazardsvc))
+        else:
+            raise ValueError("Either hazard object or hazard id + hazard type must be provided")
 
         # Get Fragility key
         fragility_key = self.get_parameter("fragility_key")
@@ -74,6 +86,7 @@ class BuildingDamage(BaseAnalysis):
                                                                               num_workers,
                                                                               inventory_args,
                                                                               repeat(retrofit_strategy),
+                                                                              repeat(hazards),
                                                                               repeat(hazard_types),
                                                                               repeat(hazard_dataset_ids))
 
@@ -105,12 +118,14 @@ class BuildingDamage(BaseAnalysis):
 
         return output_ds, output_dmg
 
-    def building_damage_analysis_bulk_input(self, buildings, retrofit_strategy, hazard_types, hazard_dataset_ids):
+    def building_damage_analysis_bulk_input(self, buildings, retrofit_strategy, hazards, hazard_types,
+                                            hazard_dataset_ids):
         """Run analysis for multiple buildings.
 
         Args:
             buildings (list): Multiple buildings from input inventory set.
             retrofit_strategy (list): building guid and its retrofit level 0, 1, 2, etc. This is Optional
+            hazards (list): List of hazard objects.
             hazard_types (list): List of Hazard type, either earthquake, tornado, or tsunami.
             hazard_dataset_ids (list): List of id of the hazard exposure.
 
@@ -130,7 +145,7 @@ class BuildingDamage(BaseAnalysis):
         multihazard_vals = {}
         adjust_demand_types_mapping = {}
 
-        for hazard_type, hazard_dataset_id in zip(hazard_types, hazard_dataset_ids):
+        for hazard, hazard_type, hazard_dataset_id in zip(hazards, hazard_types, hazard_dataset_ids):
             # get allowed demand types for the hazard type
             allowed_demand_types = [item["demand_type"].lower() for item in self.hazardsvc.get_allowed_demands(
                 hazard_type)]
@@ -172,19 +187,7 @@ class BuildingDamage(BaseAnalysis):
                 else:
                     unmapped_buildings.append(b)
 
-            if hazard_type == 'earthquake':
-                hazard_vals = self.hazardsvc.post_earthquake_hazard_values(hazard_dataset_id, values_payload)
-            elif hazard_type == 'tornado':
-                hazard_vals = self.hazardsvc.post_tornado_hazard_values(hazard_dataset_id, values_payload,
-                                                                        self.get_parameter('seed'))
-            elif hazard_type == 'tsunami':
-                hazard_vals = self.hazardsvc.post_tsunami_hazard_values(hazard_dataset_id, values_payload)
-            elif hazard_type == 'hurricane':
-                hazard_vals = self.hazardsvc.post_hurricane_hazard_values(hazard_dataset_id, values_payload)
-            elif hazard_type == 'flood':
-                hazard_vals = self.hazardsvc.post_flood_hazard_values(hazard_dataset_id, values_payload)
-            else:
-                raise ValueError("The provided hazard type is not supported yet by this analysis")
+            hazard_vals = hazard.read_hazard_values(values_payload, self.hazardsvc)
 
             # map demand type from payload to response
             # worst code I have ever written
@@ -325,13 +328,13 @@ class BuildingDamage(BaseAnalysis):
                 },
                 {
                     'id': 'hazard_type',
-                    'required': True,
+                    'required': False,
                     'description': 'Hazard Type (e.g. earthquake)',
                     'type': str
                 },
                 {
                     'id': 'hazard_id',
-                    'required': True,
+                    'required': False,
                     'description': 'Hazard ID',
                     'type': str
                 },
@@ -371,6 +374,14 @@ class BuildingDamage(BaseAnalysis):
                     'description': 'Geology dataset id',
                     'type': str,
                 }
+            ],
+            'input_hazards': [
+                {
+                    'id': 'hazard',
+                    'required': False,
+                    'description': 'Hazard object',
+                    'type': ["earthquake", "tornado", "hurricane", "flood", "tsunami"]
+                },
             ],
             'input_datasets': [
                 {
