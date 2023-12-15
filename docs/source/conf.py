ea14d106b8fa8eae5d216f085912dd59679ce9f6
Chen Wang <cwang138@illinois.edu>
2023-12-15 15:24:31 -0600

    bump version up add changelog entry

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -35,7 +35,7 @@ author = ''
 # The short X.Y version
 version = '1.15'
 # The full version, including alpha/beta/rc tags
-release = '1.15.0'
+release = '1.15.1'
 
 # -- General configuration ---------------------------------------------------
 
