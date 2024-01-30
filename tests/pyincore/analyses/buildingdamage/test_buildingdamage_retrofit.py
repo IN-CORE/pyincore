c2eea083c0ba68391b3122f84113de8aeed30792
Chen Wang <cwang138@illinois.edu>
2024-01-30 16:04:08 -0600

    update retrofit test

--- tests/pyincore/analyses/buildingdamage/test_buildingdamage_retrofit.py
+++ tests/pyincore/analyses/buildingdamage/test_buildingdamage_retrofit.py
@@ -10,75 +10,8 @@ def run_with_base_class():
     hazardsvc = HazardService(client)
 
     # dfr3 mapping
-    fragility_mapping_set_definition = {
-        "id": "N/A",
-        "name": "local joplin tornado fragility mapping object",
-        "hazardType": "tornado",
-        "inventoryType": "building",
-        'retrofitDefinitions': [
-            {
-                "name": "retrofit_method",
-                "description": "Retrofitting strategies to enhance residential wood-frame buildings from high "
-                               "wind-related hazards, such as tornadoes, hurricanes",
-                "PaperReference": {
-                    "name": "Restoration and functionality assessment of a community subjected to tornado hazard",
-                    "doi": "10.1080/15732479.2017.1354030",
-                    "yearPublished": "2018"
-                },
-                "unit": None,
-                "type": "string",
-                "allowedValues": ["1", "2", "3"],
-                "allowedNames": [
-                    "roof covering with asphalt shingles, and roof sheathing with 8d C6/12, and roof-to-wall "
-                    "connection type with two 16d toe nails",
-                    "roof covering with asphalt shingles, and roof sheathing with 8d C6/6, and roof-to-wall "
-                    "connection  type with two H2.5 clips",
-                    "roof covering with clay tiles, and roof sheathing with 8d C6/6, and roof-to-wall connection type "
-                    "with two H2.5 clips"
-                ]
-            }
-        ],
-        'mappings': [
-            {
-                "legacyEntry": {},
-                "entry": {
-                    "Non-Retrofit Fragility ID Code": "5e4ca22b9578290001e59005",
-                },
-                "rules": {
-                    "AND": [
-                        "int archetype EQUALS 1",
-                    ]
-                }
-            },
-            {
-                "legacyEntry": {},
-                "entry": {
-                    "retrofit_method": "5e4ca29e1eb25200014abc29",
-                },
-                "rules": {
-                    "AND": [
-                        "int archetype EQUALS 1",
-                        "str retrofit_method EQUALS '1'"
-                    ]
-                }
-            },
-            {
-                "legacyEntry": {},
-                "entry": {
-                    "retrofit_method": "5e4ca2f51eb25200014abc9d",
-                },
-                "rules": {
-                    "AND": [
-                        "int archetype EQUALS 1",
-                        "str retrofit_method EQUALS '2'"
-                    ]
-                }
-            },
-        ],
-        "mappingType": "fragility"
-    }
-
-    fragility_mapping_set = MappingSet(fragility_mapping_set_definition)
+    fragility_mapping_set = MappingSet.from_json_file(os.path.join(pyglobals.TEST_DATA_DIR,
+                                                                              "retrofit/tornado_retrofit_mapping.json"))
 
     # Building Damage
     # Create building damage
@@ -87,7 +20,8 @@ def run_with_base_class():
     # Load input dataset
     bldg_dataset_id = "5dbc8478b9219c06dd242c0d"  # joplin building v6 prod
     bldg_dmg.load_remote_input_dataset("buildings", bldg_dataset_id)
-    retrofit_strategy_plan1 = Dataset.from_file(os.path.join(pyglobals.TEST_DATA_DIR, "retrofit/retrofit_plan1.csv"),
+    retrofit_strategy_plan1 = Dataset.from_file(os.path.join(pyglobals.TEST_DATA_DIR,
+                                                             "retrofit/tornado_retrofit_mapping.csv"),
                                                 data_type="incore:retrofitStrategy")
     bldg_dmg.set_input_dataset("retrofit_strategy", retrofit_strategy_plan1)
 
