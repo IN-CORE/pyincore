502cf589df245f5c585a8983146f22910cbe9083
Ya-Lan Yang <ylyang@illinois.edu>
2024-03-25 11:18:36 -0500

    move logic to heatmat; fix reverted result

--- pyincore/utils/popdisloutputprocess.py
+++ pyincore/utils/popdisloutputprocess.py
@@ -46,30 +46,27 @@ class PopDislOutputProcess:
                 pd_result_flag = pd_result[(pd_result["guid"].notnull()) &
                                            (pd_result["numprec"].notnull()) &
                                            (pd_result["plcname10"] == filter_name)]
-                # only keep guid, place
+                # only keep guid and place
                 pd_result_shp = pd_result[(pd_result["guid"].notnull()) &
                                           (pd_result["numprec"].notnull()) &
                                           (pd_result["plcname10"] == filter_name)]
             else:
                 pd_result_flag = pd_result[(pd_result["guid"].notnull()) &
                                            (pd_result["numprec"].notnull())]
-                # only keep guid and dislocated
+                # only keep guid
                 pd_result_shp = pd_result[(pd_result["guid"].notnull()) &
                                           (pd_result["numprec"].notnull())]
         else:
             if filter_name:
                 pd_result_flag = pd_result[(pd_result["numprec"].notnull()) &
                                            (pd_result["plcname10"] == filter_name)]
-                # only keep guid, place
+                # only keep guid and place
                 pd_result_shp = pd_result[(pd_result["numprec"].notnull()) &
                                           (pd_result["plcname10"] == filter_name)]
             else:
                 pd_result_flag = pd_result[(pd_result["numprec"].notnull())]
                 # only keep guid
                 pd_result_shp = pd_result[(pd_result["numprec"].notnull())]
-
-        # Update "numprec" to 0 where "dislocated" is False
-        pd_result_shp.loc[pd_result_shp["dislocated"], "numprec"] = 0
 
         self.vacant_disl = vacant_disl
         self.pop_disl_result = pd_result_flag
@@ -89,7 +86,14 @@ class PopDislOutputProcess:
 
         # save as shapefile
         gdf = gpd.GeoDataFrame(df, crs='epsg:4326')
-        gdf = gdf[["guid", "numprec", "geometry"]]
+        gdf = gdf[["guid", "numprec", "geometry", "dislocated"]]
+
+        # keep original dislocated results
+        gdf["numprec_dislocated"] = gdf["numprec"].copy()
+
+        # set numprec = 0 if dislocated is False
+        gdf.loc[~gdf["dislocated"], "numprec"] = 0
+
         # set numprec as integer
         gdf["numprec"] = gdf["numprec"].fillna(0).astype(int)
         gdf.to_file(filename)
