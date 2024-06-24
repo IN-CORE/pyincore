571e9c8dd68f1af18c26fe7d2e0a30c2189887a0
Chen Wang <cwang138@illinois.edu>
2024-06-24 13:51:26 -0500

    need to preserve original functions docstring

--- pyincore/decorators.py
+++ pyincore/decorators.py
@@ -1,11 +1,20 @@
+from functools import wraps
+
+
 def forbid_offline(func):
     """
     Custom decorator to forbid method interact with remote service in offline mode.
     Returns:
     """
+
+    # uses functools.wraps to preserve the original function's metadata.
+    @wraps(func)
     def wrapper(self, *args, **kwargs):
         if self.client.offline:
             raise ValueError("Service is not available in offline mode.")
         return func(self, *args, **kwargs)
+
+    # important! Custom decorator needs to preserve the original function's docstring
+    wrapper.__doc__ = func.__doc__
 
     return wrapper
