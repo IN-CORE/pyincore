5607dfb2c8f06c8ad6f7de0dfc26102065355ab6
Chen Wang <cwang138@illinois.edu>
2024-06-26 13:33:38 -0500

    I cannot removee e203, black keeps adding it back

--- pyincore/analyses/transportationrecovery/nsga2.py
+++ pyincore/analyses/transportationrecovery/nsga2.py
@@ -4,7 +4,8 @@
 # terms of the Mozilla Public License v2.0 which accompanies this distribution,
 # and is available at https://www.mozilla.org/en-US/MPL/2.0/
 
-import sys, random
+import sys
+import random
 
 
 class Solution:
