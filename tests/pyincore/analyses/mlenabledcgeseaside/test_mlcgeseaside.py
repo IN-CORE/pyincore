160738c70692bba599e5d4fb4983590ea59c6d26
Chen Wang <cwang138@illinois.edu>
2025-05-22 17:00:11 -0500

    add base values

--- tests/pyincore/analyses/mlenabledcgeseaside/test_mlcgeseaside.py
+++ tests/pyincore/analyses/mlenabledcgeseaside/test_mlcgeseaside.py
@@ -1,14 +1,16 @@
 from pyincore import IncoreClient, globals as pyglobals
 from pyincore.analyses.mlenabledcgeseaside import MlEnabledCgeSeaside
 
-client = IncoreClient(pyglobals.INCORE_API_DEV_URL)
 
-mlcgeseaside = MlEnabledCgeSeaside(client)
+if __name__ == "__main__":
+    client = IncoreClient(pyglobals.INCORE_API_DEV_URL)
 
-sector_shocks = ""
+    mlcgeseaside = MlEnabledCgeSeaside(client)
 
-mlcgeseaside.load_remote_input_dataset("sector_shocks", sector_shocks)
-# optional
-mlcgeseaside.set_parameter("result_name", "seaside")
+    sector_shocks = "682f9d9fdf347312a143a245"
 
-mlcgeseaside.run_analysis()
+    mlcgeseaside.load_remote_input_dataset("sector_shocks", sector_shocks)
+    # optional
+    mlcgeseaside.set_parameter("result_name", "seaside")
+
+    mlcgeseaside.run_analysis()
